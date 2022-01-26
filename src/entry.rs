410ff54842365c7c7661cf3161139529c02dd804
Sungbae Yoo <sungbae.yoo@samsung.com>
2022-01-26 16:38:33 +0900

    Remove license from source files
    Signed-off-by: Sungbae Yoo <sungbae.yoo@samsung.com>

--- src/entry.rs
+++ src/entry.rs
@@ -1,21 +1,3 @@
-/*
- * Copyright (c) 2022 Samsung Electronics Co., Ltd. All Rights Reserved.
- *
- * PROPRIETARY/CONFIDENTIAL
- * This software is the confidential and proprietary information of
- * Samsung Electronics Co., Ltd. ("Confidential Information").
- * You shall not disclose such Confidential Information and
- * shall use it only in accordance with the terms of the license agreement
- * you entered into with Samsung Electronics Co., Ltd. (“SAMSUNG”).
- * SAMSUNG MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT
- * THE SUITABILITY OF THE SOFTWARE, EITHER EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE,
- * OR NON-INFRINGEMENT. SAMSUNG SHALL NOT BE LIABLE
- * FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING,
- * MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
- */
-
 use realm_management_monitor::io::{stdout, Write};
 
 use crate::config::RMM_STACK_SIZE;

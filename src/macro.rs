7ac0362f9286c66312f483eb9e6c4ec37530bfa2
Sungbae Yoo <sungbae.yoo@samsung.com>
2022-01-27 15:48:17 +0900

    Add implementation of print macros
    Signed-off-by: Sungbae Yoo <sungbae.yoo@samsung.com>

--- src/macro.rs
+++ src/macro.rs
@@ -1,59 +1,89 @@
 #[macro_export]
 macro_rules! print {
-    ($($arg:tt)*) => {};
+    ($($arg:tt)*) => {
+		#[allow(unused_extern_crates)]
+        extern crate alloc;
+		#[allow(unused_imports)]
+        use core::fmt::Write;
+        use $crate::io::Write as IoWrite;
+        let buffer = alloc::format!($($arg)*);
+        let _ = unsafe {
+			$crate::io::stdout()
+		 }.write_all(buffer.as_bytes());
+    };
 }
 
 #[macro_export]
 macro_rules! println {
-    () => {};
-    ($fmt:expr) => {};
-    ($fmt:expr, $($arg:tt)*) => {};
+    () => {$crate::print!("\n")};
+    ($fmt:expr) => {$crate::print!(concat!($fmt, "\n"))};
+    ($fmt:expr, $($arg:tt)*) => {$crate::print!(concat!($fmt, "\n"), $($arg)*)};
 }
 
 #[macro_export]
 macro_rules! eprint {
-    ($fmt:expr) => {};
-    ($fmt:expr, $($arg:tt)*) => {};
+    ($fmt:expr) => {
+		#[allow(unused_extern_crates)]
+        extern crate alloc;
+		#[allow(unused_imports)]
+        use core::fmt::Write;
+        use $crate::io::Write as IoWrite;
+        let buffer = concat!("\x1b[0;31m", $fmt, "\x1b[0m");
+        let _ = unsafe {
+			$crate::io::stdout()
+		 }.write_all(buffer.as_bytes());
+
+	};
+    ($fmt:expr, $($arg:tt)*) => {{
+        extern crate alloc;
+		#[allow(unused_imports)]
+        use core::fmt::Write;
+        use $crate::io::Write as IoWrite;
+        let buffer = alloc::format!(concat!("\x1b[0;31m", $fmt, "\x1b[0m"), $($arg)*);
+        let _ = unsafe {
+			$crate::io::stdout()
+		 }.write_all(buffer.as_bytes());
+    }};
 }
 
 #[macro_export]
 macro_rules! eprintln {
-    () => {};
-    ($fmt:expr) => {};
-    ($fmt:expr, $($arg:tt)*) => {};
+    () => {$crate::eprint!("\n")};
+    ($fmt:expr) => {$crate::eprint!(concat!($fmt, "\n"))};
+    ($fmt:expr, $($arg:tt)*) => {$crate::eprint!(concat!($fmt, "\n"), $($arg)*)};
 }
 
 #[cfg(test)]
 mod test {
-	use crate::{println, eprintln};
+    use crate::{eprintln, println};
 
-	#[test]
-	fn println_without_arg() {
-		println!();
-	}
+    #[test]
+    fn println_without_arg() {
+        println!();
+    }
 
-	#[test]
-	fn println_without_format() {
-		println!("hello");
-	}
+    #[test]
+    fn println_without_format() {
+        println!("hello");
+    }
 
-	#[test]
-	fn println_with_format() {
-		println!("number {}", 1234);
-	}
+    #[test]
+    fn println_with_format() {
+        println!("number {}", 1234);
+    }
 
-	#[test]
-	fn eprintln_without_arg() {
-		eprintln!();
-	}
+    #[test]
+    fn eprintln_without_arg() {
+        eprintln!();
+    }
 
-	#[test]
-	fn eprintln_without_format() {
-		eprintln!("hello");
-	}
+    #[test]
+    fn eprintln_without_format() {
+        eprintln!("hello");
+    }
 
-	#[test]
-	fn eprintln_with_format() {
-		eprintln!("number {}", 4321);
-	}
+    #[test]
+    fn eprintln_with_format() {
+        eprintln!("number {}", 4321);
+    }
 }

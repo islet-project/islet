cddde47782ede8d95671d88448d2de6cf314f6c4
Bokdeuk Jeong <bd.jeong@samsung.com>
2022-03-10 10:31:19 +0900

    set PAGE_SIZE with PAGE_BITS

--- src/config.rs
+++ src/config.rs
@@ -4,8 +4,8 @@ pub const NUM_OF_CPU: usize = 8;
 pub const NUM_OF_CLUSTER: usize = 2;
 pub const NUM_OF_CPU_PER_CLUSTER: usize = NUM_OF_CPU / NUM_OF_CLUSTER;
 
-pub const PAGE_SIZE: usize = 4096;
 pub const PAGE_BITS: usize = 12;
+pub const PAGE_SIZE: usize = 1 << PAGE_BITS;
 
 pub const RMM_STACK_SIZE: usize = 1024 * 1024;
 pub const RMM_HEAP_SIZE: usize = 8 * 1024 * 1024;

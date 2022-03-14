a084e327a030a291055948850997aa8b103576c7
Sungbae Yoo <sungbae.yoo@samsung.com>
2022-03-14 14:56:46 +0900

    ci: Fix build break of documentation due to rust version mismatching
    Signed-off-by: Sungbae Yoo <sungbae.yoo@samsung.com>

--- scripts/make_doc.sh
+++ scripts/make_doc.sh
@@ -4,7 +4,9 @@ ROOT=$(git rev-parse --show-toplevel)
 
 source ${ROOT}/scripts/env.sh
 
+mv ${ROOT}/rust-toolchain ${ROOT}/rust-toolchain.bak
 cargo install mdbook mdbook-plantuml
+mv ${ROOT}/rust-toolchain.bak ${ROOT}/rust-toolchain
 
 (
 	cd ${ROOT}/docs

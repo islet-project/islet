dd7c8204b68b0e9e94517978496d85379a4a3ffa
Sujin Park <s800.park@samsung.com>
2022-03-16 20:29:33 +0900

    Add offsetof macro

--- rmm/core/src/macro.rs
+++ rmm/core/src/macro.rs
@@ -18,8 +18,7 @@ macro_rules! eprint {
     ($fmt:expr) => {
         let buffer = concat!("\x1b[0;31m", $fmt, "\x1b[0m");
         let _ = $crate::io::stdout().write_all(buffer.as_bytes());
-
-	};
+    };
     ($fmt:expr, $($arg:tt)*) => {{
         let buffer = alloc::format!(concat!("\x1b[0;31m", $fmt, "\x1b[0m"), $($arg)*);
         let _ = $crate::io::stdout().write_all(buffer.as_bytes());
@@ -31,6 +30,20 @@ macro_rules! eprintln {
     () => {$crate::eprint!("\n")};
     ($fmt:expr) => {$crate::eprint!(concat!($fmt, "\n"))};
     ($fmt:expr, $($arg:tt)*) => {$crate::eprint!(concat!($fmt, "\n"), $($arg)*)};
+}
+
+#[macro_export]
+macro_rules! offset_of {
+    ($Struct:path, $field:ident) => {{
+        fn offset() -> usize {
+            let u = core::mem::MaybeUninit::<$Struct>::uninit();
+            let &$Struct { $field: ref f, .. } = unsafe { &*u.as_ptr() };
+            let o = (f as *const _ as usize).wrapping_sub(&u as *const _ as usize);
+            assert!((0..=core::mem::size_of_val(&u)).contains(&o));
+            o
+        }
+        offset()
+    }};
 }
 
 #[cfg(test)]
@@ -104,5 +117,24 @@ mod test {
             unsafe { (*mock_ptr).output() },
             "\x1b[0;31mnumber 4321\n\x1b[0m"
         );
+    }
+
+    #[test]
+    fn offset_of() {
+        struct Context {
+            gp_regs: [u64; 31],
+            elr: u64,
+            spsr: u64,
+            sys_regs: SystemRegister,
+        }
+        struct SystemRegister {
+            pub ttbr0: u64,
+            pub tpidr: u64,
+        }
+
+        assert_eq!(offset_of!(Context, gp_regs), 0);
+        assert_eq!(offset_of!(Context, elr), 248);
+        assert_eq!(offset_of!(Context, spsr), 256);
+        assert_eq!(offset_of!(Context, sys_regs), 264);
     }
 }

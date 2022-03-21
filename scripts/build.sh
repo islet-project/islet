cfceac87270b9fb456739d5dced9f8aa5c22010c
Sungbae Yoo <sungbae.yoo@samsung.com>
2022-03-21 22:26:38 +0900

    Fix build/run.sh for test vm-image only
    Signed-off-by: Sungbae Yoo <sungbae.yoo@samsung.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,10 +9,27 @@ source ${ROOT}/scripts/env.sh
 	make CROSS_COMPILE=${CROSS_COMPILE} PLAT=fvp DEBUG=1
 )
 
+if [ ! -f "${FIPTOOL}" ]; then
+	cd ${TRUSTED_FIRMWARE_A}/tools/fiptool
+	make
+fi
+
 (
 	cd ${VM_IMAGE}
 	make CROSS_COMPILE=${CROSS_COMPILE} PLAT=fvp DEBUG=1
 	cp ${VM_IMAGE}/build/fvp/debug/tftf.bin ${ROOT}/out/vm-image.bin
+
+	${FIPTOOL} create \
+		--fw-config ${TRUSTED_FIRMWARE_A}/build/fvp/debug/fdts/fvp_fw_config.dtb \
+		--tb-fw-config ${TRUSTED_FIRMWARE_A}/build/fvp/debug/fdts/fvp_tb_fw_config.dtb \
+		--soc-fw-config ${TRUSTED_FIRMWARE_A}/build/fvp/debug/fdts/fvp_soc_fw_config.dtb \
+		--nt-fw-config ${TRUSTED_FIRMWARE_A}/build/fvp/debug/fdts/fvp_nt_fw_config.dtb \
+		--hw-config ${TRUSTED_FIRMWARE_A}/build/fvp/debug/fdts/fvp-base-gicv3-psci-1t.dtb \
+		--tb-fw ${TRUSTED_FIRMWARE_A}/build/fvp/debug/bl2.bin \
+		--soc-fw ${TRUSTED_FIRMWARE_A}/build/fvp/debug/bl31.bin \
+		--rmm-fw ${TRUSTED_FIRMWARE_A}/build/fvp/debug/rmm.bin \
+		--nt-fw ${ROOT}/out/vm-image.bin \
+		${ROOT}/out/fip-vm-image.bin
 )
 
 (
@@ -26,11 +43,6 @@ source ${ROOT}/scripts/env.sh
 	cargo build --release
 	${CROSS_COMPILE}objcopy -O binary ${ROOT}/out/aarch64-unknown-none-softfloat/release/rmm ${ROOT}/out/aarch64-unknown-none-softfloat/release/rmm.bin
 )
-
-if [ ! -f "${FIPTOOL}" ]; then
-	cd ${TRUSTED_FIRMWARE_A}/tools/fiptool
-	make
-fi
 
 #Make fip.bin
 ${FIPTOOL} create \
--- scripts/run.sh
+++ scripts/run.sh
@@ -8,11 +8,11 @@ source ${HERE}/env.sh
 (
 	cd ${FASTMODEL}/Base_RevC_AEMvA_pkg/models/Linux64_GCC-6.4/
 	case "$1" in
-		--vm-start-test)
+		--vm-image-test)
+			shift
 			./FVP_Base_RevC-2xAEMvA \
-				-C bp.flashloader0.fname=${ROOT}/out/fip.bin \
+				-C bp.flashloader0.fname=${ROOT}/out/fip-vm-image.bin \
 				-C bp.secureflashloader.fname=${ROOT}/out/bl1.bin \
-				--data="${ROOT}/out/vm-image.bin"@0x88066000 \
 				-f ${HERE}/.config \
 				-Q 1000 "$@"
 			;;
@@ -20,9 +20,9 @@ source ${HERE}/env.sh
 			./FVP_Base_RevC-2xAEMvA \
 				-C bp.flashloader0.fname=${ROOT}/out/fip.bin \
 				-C bp.secureflashloader.fname=${ROOT}/out/bl1.bin \
+				--data="${ROOT}/out/vm-image.bin"@0x88066000 \
 				-f ${HERE}/.config \
 				-Q 1000 "$@"
-			exit
 			;;
 	esac
 )

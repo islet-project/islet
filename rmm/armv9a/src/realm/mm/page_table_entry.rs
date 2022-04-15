997d06af34858062d84f0d646fddd99209e064e1
Sungbae Yoo <sungbae.yoo@samsung.com>
2022-04-15 19:51:15 +0900

    Add explicit data cache clean for IPA page table update
    Signed-off-by: Sungbae Yoo <sungbae.yoo@samsung.com>

--- rmm/armv9a/src/realm/mm/page_table_entry.rs
+++ rmm/armv9a/src/realm/mm/page_table_entry.rs
@@ -69,5 +69,14 @@ impl PageTableEntry {
             .set_masked_value(RawPTE::SH, pte_shareable::INNER)
             .set_bits(RawPTE::AF)
             .set_bits(RawPTE::VALID);
+
+        unsafe {
+            llvm_asm! {"
+            dsb ishst
+            dc civac, $0
+            dsb ish
+            isb
+            " : : "r"(&self.0 as *const _ as usize)};
+        }
     }
 }

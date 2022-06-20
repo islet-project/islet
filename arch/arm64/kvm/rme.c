c1f68cbfa054a980106536849234e3be33d5b07d
Steven Price <steven.price@arm.com>
2022-06-20 11:54:11 +0100

    KVM: arm64: Allow activating realms
    Add the ioctl to activate a realm and set the static branch to enable
    access to the realm functionality if the RMM is detected.
    
    Signed-off-by: Steven Price <steven.price@arm.com>

--- arch/arm64/kvm/rme.c
+++ arch/arm64/kvm/rme.c
@@ -1000,6 +1000,20 @@ static int kvm_init_ipa_range_realm(struct kvm *kvm,
 	return ret;
 }
 
+static int kvm_activate_realm(struct kvm *kvm)
+{
+	struct realm *realm = &kvm->arch.realm;
+
+	if (kvm_realm_state(kvm) != REALM_STATE_NEW)
+		return -EBUSY;
+
+	if (rmi_realm_activate(virt_to_phys(realm->rd)))
+		return -ENXIO;
+
+	WRITE_ONCE(realm->state, REALM_STATE_ACTIVE);
+	return 0;
+}
+
 /* Protects access to rme_vmid_bitmap */
 static DEFINE_SPINLOCK(rme_vmid_lock);
 static unsigned long *rme_vmid_bitmap;
@@ -1175,6 +1189,9 @@ int kvm_realm_enable_cap(struct kvm *kvm, struct kvm_enable_cap *cap)
 		r = kvm_populate_realm(kvm, &args);
 		break;
 	}
+	case KVM_CAP_ARM_RME_ACTIVATE_REALM:
+		r = kvm_activate_realm(kvm);
+		break;
 	default:
 		r = -EINVAL;
 		break;
@@ -1415,7 +1432,7 @@ int kvm_init_rme(void)
 
 	WARN_ON(rmi_features(0, &rmm_feat_reg0));
 
-	/* Future patch will enable static branch kvm_rme_is_available */
+	static_branch_enable(&kvm_rme_is_available);
 
 	return 0;
 }

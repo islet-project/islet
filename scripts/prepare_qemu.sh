8ff4cc26850d330b3b569a67de009871752234df
Beom Heyn Kim <beomheyn.kim@samsung.com>
2022-07-01 16:16:59 +0900

    changes scripts to fix a bug downloading toolchains

--- scripts/prepare_qemu.sh
+++ scripts/prepare_qemu.sh
@@ -4,7 +4,7 @@ ROOT=$(git rev-parse --show-toplevel)
 
 cd ${ROOT}/assets/prebuilt/qemu
 if [ ! -f qemu-system-aarch64 ]; then
-    echo "qemu-system-aarch64.zip does NOT exist"
+    echo "qemu-system-aarch64 does NOT exist"
     echo "Unzipping qemu-system-aarch64.zip"
     unzip qemu-system-aarch64.zip
 else

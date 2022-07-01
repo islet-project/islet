c6a01c52a2184d46d1b4746584853c90687445c5
Beom Heyn Kim <beomheyn.kim@samsung.com>
2022-07-01 15:18:13 +0900

    put all prerequisite scripts in init.sh

--- scripts/prepare_qemu.sh
+++ scripts/prepare_qemu.sh
@@ -2,6 +2,11 @@
 
 ROOT=$(git rev-parse --show-toplevel)
 
-cd assets/prebuilt/qemu/
-unzip qemu-system-aarch64.zip
-
+cd ${ROOT}/assets/prebuilt/qemu
+if [ ! -f qemu-system-aarch64 ]; then
+    echo "qemu-system-aarch64.zip does NOT exist"
+    echo "Unzipping qemu-system-aarch64.zip"
+    unzip qemu-system-aarch64.zip
+else
+    echo "qemu-system-aarch64.zip already exists"
+fi

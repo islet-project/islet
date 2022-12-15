015ae374dc4ce1f35f030280ee4129d21a96efa7
Sangwan Kwon <sangwan.kwon@samsung.com>
2022-12-15 15:02:11 +0900

    scripts: Use third-party/tf-a-tests path
    Signed-off-by: Sangwan Kwon <sangwan.kwon@samsung.com>

--- scripts/config.py
+++ scripts/config.py
@@ -9,11 +9,15 @@ PREBUILT = os.path.join(ROOT, "assets/prebuilt")
 
 RMM = os.path.join(ROOT, "rmm/board/fvp")
 SDK = os.path.join(ROOT, "sdk/")
-TF_A_TESTS = os.path.join(ROOT, "tf-a-tests")
-TRUSTED_FIRMWARE_A = os.path.join(ROOT, "third-party/tf-a")
-BUILD_SCRIPT = os.path.join(ROOT, "third-party/optee-build")
-GUEST_LINUX = os.path.join(ROOT, "third-party/realm-linux")
-NW_LINUX = os.path.join(ROOT, "third-party/nw-linux")
+
+# third-party
+THIRD_PARTY = os.path.join(ROOT, "third-party")
+
+BUILD_SCRIPT = os.path.join(THIRD_PARTY, "optee-build")
+GUEST_LINUX = os.path.join(THIRD_PARTY, "realm-linux")
+NW_LINUX = os.path.join(THIRD_PARTY, "nw-linux")
+TF_A = os.path.join(THIRD_PARTY, "tf-a")
+TF_A_TESTS = os.path.join(THIRD_PARTY, "tf-a-tests")
 
 # directory shared between the pc desktop and the host OS on fvp using 9p filesystem
 PC_SHARE_DIR = os.path.join(OUT, "pc_share_dir")
@@ -21,4 +25,4 @@ PC_SHARE_DIR = os.path.join(OUT, "pc_share_dir")
 CROSS_COMPILE = os.path.join(ROOT, "assets/toolchains/aarch64/bin/aarch64-none-linux-gnu-")
 LINUX_CROSS_COMPILE = os.path.join(ROOT, "assets/toolchains/aarch64/bin/aarch64-linux-gnu-")
 FASTMODEL = os.path.join(ROOT, "assets/fastmodel/Base_RevC_AEMvA_pkg/models/Linux64_GCC-9.3")
-FIPTOOL = os.path.join(TRUSTED_FIRMWARE_A, "tools/fiptool")
+FIPTOOL = os.path.join(TF_A, "tools/fiptool")

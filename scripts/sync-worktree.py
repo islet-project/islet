3ab84fb4d5d4a166dbe17cf2b47c595b68cdd419
Sangwan Kwon <sangwan.kwon@samsung.com>
2022-12-23 13:55:48 +0900

    third-party: Manage project list by worktree.toml
    Signed-off-by: Sangwan Kwon <sangwan.kwon@samsung.com>

--- scripts/sync-worktree.py
+++ scripts/sync-worktree.py
@@ -4,6 +4,7 @@ import glob
 import os
 import subprocess
 import sys
+import toml
 
 from config import ROOT, THIRD_PARTY
 
@@ -18,16 +19,18 @@ def branch_name(path):
     proc = run(['git', 'rev-parse', '--abbrev-ref', 'HEAD'], cwd=path)
     return proc.stdout.strip()
 
-def sync():
-    third_parties = []
-    for path in glob.glob(os.path.join(THIRD_PARTY, "*/")):
-        name = os.path.basename(path.rstrip("/"))
-        run(['git', 'rm', '--cached', f'third-party/{name}'], cwd=ROOT)
+def add_worktree(project, branch):
+    print(f"[!] Sync worktree to third-party/{project}: branch[{branch}]")
+    run(['git', 'rm', '--cached', f'third-party/{project}'], cwd=ROOT)
 
-        proc = run(['git', 'worktree', 'add', '--guess-remote', f'third-party/{name}'], cwd=ROOT)
-        if proc.returncode != 0 and name != branch_name(path):
-            print(f"Please remove local branch [{name}] to fetch it from remote.")
-            sys.exit(1)
+    proc = run(['git', 'worktree', 'add', '--guess-remote', f'third-party/{branch}'], cwd=ROOT)
+    if proc.returncode != 0 and branch != branch_name(os.path.join(THIRD_PARTY, project)):
+        print(f"[!] Please remove local branch [{branch}] to fetch it from remote.")
+        sys.exit(1)
+
+    run(['git', 'worktree', 'move', branch, f'third-party/{project}'], cwd=ROOT)
 
 if __name__ == "__main__":
-    sync()
+    tree = toml.load(os.path.join(THIRD_PARTY, "worktree.toml"))
+    for project, info in tree.items():
+        add_worktree(project, info["branch"])

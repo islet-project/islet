6700a717c17a6188952895767d9358b9d3e7f1f0
Sangwan Kwon <sangwan.kwon@samsung.com>
2022-12-26 18:00:22 +0900

    scripts: shell: Formatting
    Signed-off-by: Sangwan Kwon <sangwan.kwon@samsung.com>

--- scripts/prepare_fastmodel.sh
+++ scripts/prepare_fastmodel.sh
@@ -4,15 +4,15 @@ ROOT=$(git rev-parse --show-toplevel)
 
 cd ${ROOT}/assets
 if [ ! -d fastmodel ]; then
-    mkdir fastmodel
+	mkdir fastmodel
 fi
 
 cd ${ROOT}/assets/fastmodel
 if [ ! -f FVP_Base_RevC-2xAEMvA_11.18_16_Linux64.tgz ]; then
-    echo "FVP_Base_RevC-2xAEMvA_11.18_16_Linux64.tgz does NOT exist"
-    wget https://developer.arm.com/-/media/Files/downloads/ecosystem-models/FVP_Base_RevC-2xAEMvA_11.18_16_Linux64.tgz
-    echo "Extracting FVP_Base_RevC-2xAEMvA_11.18_16_Linux64.tgz"
-    tar -xzf FVP_Base_RevC-2xAEMvA_11.18_16_Linux64.tgz
+	echo "FVP_Base_RevC-2xAEMvA_11.18_16_Linux64.tgz does NOT exist"
+	wget https://developer.arm.com/-/media/Files/downloads/ecosystem-models/FVP_Base_RevC-2xAEMvA_11.18_16_Linux64.tgz
+	echo "Extracting FVP_Base_RevC-2xAEMvA_11.18_16_Linux64.tgz"
+	tar -xzf FVP_Base_RevC-2xAEMvA_11.18_16_Linux64.tgz
 else
-    echo "FVP_Base_RevC-2xAEMvA_11.18_16_Linux64.tgz already exists"
+	echo "FVP_Base_RevC-2xAEMvA_11.18_16_Linux64.tgz already exists"
 fi
--- scripts/prepare_qemu.sh
+++ scripts/prepare_qemu.sh
@@ -4,9 +4,9 @@ ROOT=$(git rev-parse --show-toplevel)
 
 cd ${ROOT}/assets/prebuilt/qemu
 if [ ! -f qemu-system-aarch64 ]; then
-    echo "qemu-system-aarch64 does NOT exist"
-    echo "Unzipping qemu-system-aarch64.zip"
-    unzip qemu-system-aarch64.zip
+	echo "qemu-system-aarch64 does NOT exist"
+	echo "Unzipping qemu-system-aarch64.zip"
+	unzip qemu-system-aarch64.zip
 else
-    echo "qemu-system-aarch64.zip already exists"
+	echo "qemu-system-aarch64.zip already exists"
 fi

db4fbe9ad219aa48a494b5c356fef82bca75d17e
Sangwan Kwon <sangwan.kwon@samsung.com>
2023-02-22 11:22:00 +0900

    scripts: Parse argv to add single project
    $ ./scripts/sync-worktree.py kvmtool
    
    Signed-off-by: Sangwan Kwon <sangwan.kwon@samsung.com>

--- scripts/sync-worktree.py
+++ scripts/sync-worktree.py
@@ -25,21 +25,25 @@ def commit(path):
 
 def add_worktree(project, info):
     branch = info["branch"]
-    print(f"[!] Sync worktree to third-party/{project}: branch[{branch}]")
+    print(f"[+] Sync worktree to third-party/{project}: branch[{branch}]")
 
     proc = run(['git', 'worktree', 'add', '--guess-remote', f'third-party/{branch}'], cwd=ROOT)
     print(proc)
     if proc.returncode != 0 and branch != branch_name(os.path.join(THIRD_PARTY, project)):
-        print(f"[!] Please remove local branch [{branch}] to fetch it from remote.")
+        print(f"[-] Please remove local branch [{branch}] to fetch it from remote.")
         sys.exit(1)
 
     run(['git', 'worktree', 'move', branch, f'third-party/{project}'], cwd=ROOT)
  
     if info["commit"] != commit(os.path.join(THIRD_PARTY, project)):
-        print(f"[!] Mismatched [{project}] commit: {commit}")
+        print(f"[-] Mismatched [{project}] commit: {commit}")
         sys.exit(1)
 
 if __name__ == "__main__":
     tree = toml.load(os.path.join(THIRD_PARTY, "worktree.toml"))
+
     for project, info in tree.items():
-        add_worktree(project, info)
+        if len(sys.argv) == 1:
+            add_worktree(project, info)
+        elif len(sys.argv) == 2 and sys.argv[1] == project:
+            add_worktree(project, info)

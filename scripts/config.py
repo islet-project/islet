926150577bcc3b9d6e50446c8cd9e26816533d57
Sangwan Kwon <sangwan.kwon@samsung.com>
2023-02-27 15:24:36 +0900

    realm: Launch linux realm with linux-cca
    [components]
    - fvp
    - linux-cca(host, guest)
    - kvmtool
    
    [commands]
    - Run linux-cca on fvp
      $ ./scripts/fvp-cca -nw=linux -rm=linux -rmm=tf-rmm
    - Launch realm
      $ cd guest
      $ ./launch.sh
    
    [troubleshooting]
    - Both tf-rmm version (v0.2.0 and latest) are not working
      => Use 3rd-tf-rmm-230222 branch
    - tf-a version v2.8.0-lts is not working
      => Use latest version
    - kvmtool built with gcc 11.3 or gcc 9.4 are not working
      => Use 10.2 version
    
    Signed-off-by: Sangwan Kwon <sangwan.kwon@samsung.com>

--- scripts/config.py
+++ scripts/config.py
@@ -4,12 +4,17 @@ import os
 
 ROOT = os.path.realpath(os.path.join(os.path.dirname(__file__), ".."))
 OUT = os.path.join(ROOT, "out")
-CONFIG = os.path.join(ROOT, "scripts/.config")
+
+SCRIPT = os.path.join(ROOT, "scripts")
+CONFIG = os.path.join(SCRIPT, ".config")
+LAUNCH_REALM = os.path.join(SCRIPT, "launch_realm_on_fvp.sh")
 
 PREBUILT = os.path.join(ROOT, "assets/prebuilt")
 PREBUILT_EDK2 = os.path.join(PREBUILT, "FVP_AARCH64_EFI.fd")
 PREBUILT_GRUB = os.path.join(PREBUILT, "bootaa64.efi")
 PREBUILT_QEMU = os.path.join(PREBUILT, "qemu")
+
+REALM_ROOTFS = os.path.join(ROOT, "assets/rootfs")
 
 REALM = os.path.join(ROOT, "realm")
 RMM = os.path.join(ROOT, "rmm/board/fvp")
@@ -19,16 +24,20 @@ SDK = os.path.join(ROOT, "sdk/")
 THIRD_PARTY = os.path.join(ROOT, "third-party")
 
 BUILD_SCRIPT = os.path.join(THIRD_PARTY, "optee-build")
-GUEST_LINUX = os.path.join(THIRD_PARTY, "realm-linux")
+REALM_LINUX = os.path.join(THIRD_PARTY, "realm-linux")
 NW_LINUX = os.path.join(THIRD_PARTY, "nw-linux")
 TF_A = os.path.join(THIRD_PARTY, "tf-a")
 TF_A_TESTS = os.path.join(THIRD_PARTY, "tf-a-tests")
+TFTF_BIN = os.path.join(TF_A_TESTS, "build/fvp/debug/tftf.bin")
+TF_RMM = os.path.join(THIRD_PARTY, "tf-rmm")
+KVMTOOL = os.path.join(THIRD_PARTY, "kvmtool")
 
 # directory shared between the pc desktop and the host OS on fvp using 9p filesystem
 PC_SHARE_DIR = os.path.join(OUT, "pc_share_dir")
+GUEST_SHARED = os.path.join(PC_SHARE_DIR, "guest")
 
-#CROSS_COMPILE = os.path.join(ROOT, "assets/toolchains/aarch64/bin/aarch64-none-linux-gnu-")
-CROSS_COMPILE = os.path.join(ROOT, "assets/gcc/arm-gnu-toolchain-11.3.rel1-x86_64-aarch64-none-elf/bin/aarch64-none-elf-")
-LINUX_CROSS_COMPILE = os.path.join(ROOT, "assets/toolchains/aarch64/bin/aarch64-linux-gnu-")
+DTC = os.path.join(ROOT, "assets/dtc")
+CROSS_COMPILE = os.path.join(ROOT, "assets/toolchain/aarch64-none-elf/bin/aarch64-none-elf-")
+LINUX_CROSS_COMPILE = os.path.join(ROOT, "assets/toolchain/aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-")
+KVMTOOL_CROSS_COMPILE = os.path.join(ROOT, "assets/toolchain/aarch64-none-linux-gnu-10-2/bin/aarch64-none-linux-gnu-")
 FASTMODEL = os.path.join(ROOT, "assets/fastmodel/Base_RevC_AEMvA_pkg/models/Linux64_GCC-9.3")
-FIPTOOL = os.path.join(TF_A, "tools/fiptool")
---
+++ scripts/launch_realm_on_fvp.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+./lkvm run \
+	--debug \
+	--realm \
+	--measurement-algo="sha256" \
+	--disable-sve \
+	--console serial \
+	--irqchip=gicv3 \
+	-m 256M \
+	-c 1 \
+	-k linux.realm \
+	-i rootfs-realm.cpio.gz \
+	-p "earlycon=ttyS0 printk.devkmsg=on"

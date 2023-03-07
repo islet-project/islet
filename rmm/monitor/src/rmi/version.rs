b6b198ad5312422322596a6c9134539d8b36784c
Sangwan Kwon <sangwan.kwon@samsung.com>
2023-03-07 15:46:23 +0900

    rmm: Set default loglevel to trace
    Signed-off-by: Sangwan Kwon <sangwan.kwon@samsung.com>

--- rmm/monitor/src/rmi/version.rs
+++ rmm/monitor/src/rmi/version.rs
@@ -8,8 +8,8 @@ extern crate alloc;
 
 pub fn set_event_handler(mainloop: &mut Mainloop<rmi::Receiver>) {
     listen!(mainloop, rmi::Code::Version, |call| {
-        trace!("RMM: requested version information");
         let config = config::instance().ok_or(Error::new(ErrorKind::Unsupported))?;
+        trace!("RMM: requested version information: {}", config.abi_version());
         Ok(call.reply(config.abi_version())?)
     });
 }

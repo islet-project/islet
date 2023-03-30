add52afcfad247dd321a411fca3f08a6c98e22e4
Sangwan Kwon <sangwan.kwon@samsung.com>
2023-03-30 18:45:42 +0900

    rmm: Enter to realm with rmi::REC_ENTER
    rsi::HOST_CALL is used for realm_exit
    
    Signed-off-by: Sangwan Kwon <sangwan.kwon@samsung.com>

--- rmm/monitor/src/rmi/rtt.rs
+++ rmm/monitor/src/rmi/rtt.rs
@@ -13,9 +13,19 @@ pub fn set_event_handler(mainloop: &mut Mainloop) {
     // related with:
     //   - RIM
     //   - REALM_MAP_MEMORY
-    listen!(mainloop, rmi::DATA_CREATE, |ctx, _, _| {
-        super::dummy();
-        ctx.ret[0] = rmi::SUCCESS;
+    listen!(mainloop, rmi::DATA_CREATE, |ctx, rmi, _| {
+        let data_addr = ctx.arg[0];
+        let _rd_addr = ctx.arg[1];
+        let _map_addr = ctx.arg[2];
+        let src_addr = ctx.arg[3];
+
+        let realm_id = 0; // temporarily
+        let granule_sz = 4096;
+        let ret = rmi.map(realm_id, data_addr, src_addr, granule_sz, 0);
+        match ret {
+            Ok(_) => ctx.ret[0] = rmi::SUCCESS,
+            Err(_) => ctx.ret[0] = rmi::RET_FAIL,
+        }
     });
 
     // related with:

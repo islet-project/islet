2a7c20996a1f0972449b1e738ed829b4e5eacd44
Sangwan Kwon <sangwan.kwon@samsung.com>
2023-04-20 16:04:46 +0900

    realm: Make hvc-loop simplest
    tf-a-tests should be patched to pack hvc-loop
    $ ./scripts/fvp-cca -nw=tf-a-tests -rm=hvc-loop
    
    Signed-off-by: Sangwan Kwon <sangwan.kwon@samsung.com>

--- realm/hvc-loop/src/main.rs
+++ realm/hvc-loop/src/main.rs
@@ -1,18 +1,20 @@
 #![no_std]
 #![no_main]
 #![feature(asm_const)]
-#![feature(naked_functions)]
 #![warn(rust_2018_idioms)]
 
-#[macro_use]
-extern crate log;
-
 mod entry;
+mod panic;
 
 #[no_mangle]
 pub unsafe fn main() -> ! {
+    let mut i: usize = 0;
     loop {
-        // core::arch::asm!("hvc #0x0");
-        info!("Launched on normal world.");
+        core::arch::asm! {
+            "mov x0, {}",
+            "hvc #0",
+            in(reg) i,
+        };
+        i += 1;
     }
 }

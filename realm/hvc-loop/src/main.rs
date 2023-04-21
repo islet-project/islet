e2b82f028caa790190ed98a7d58dd2d63e87139c
Sangwan Kwon <sangwan.kwon@samsung.com>
2023-04-21 15:30:39 +0900

    realm: Make hvc-loop realm runnable with tf-rmm
    $ ./scripts/fvp-cca -nw=tf-a-tests -rmm=tf-rmm -rm=hvc-loop
    
    Signed-off-by: Sangwan Kwon <sangwan.kwon@samsung.com>

--- realm/hvc-loop/src/main.rs
+++ realm/hvc-loop/src/main.rs
@@ -6,15 +6,61 @@
 mod entry;
 mod panic;
 
+use core::arch::asm;
+
 #[no_mangle]
 pub unsafe fn main() -> ! {
-    let mut i: usize = 0;
-    loop {
-        core::arch::asm! {
-            "mov x0, {}",
-            "hvc #0",
-            in(reg) i,
-        };
-        i += 1;
+    // CHECK:
+    //   HOST_CALL is not initialized when use tf-rmm
+    //   HOST_CALL is initialized when use islet-rmm
+    HOST_CALL.padding = 0;
+
+    HOST_CALL.imm = 1;
+    let arg = [
+        &HOST_CALL as *const _ as usize,
+        HOST_CALL.imm as usize,
+        0,
+        0,
+    ];
+    let ret = smc(RSI_HOST_CALL, arg);
+
+    HOST_CALL.imm = 2;
+    let arg = [
+        &HOST_CALL as *const _ as usize,
+        HOST_CALL.imm as usize,
+        0,
+        0,
+    ];
+    let ret = smc(RSI_HOST_CALL, arg);
+
+    loop {}
+}
+
+// TODO:
+//   Detach rmm-spec(data structures & commands) to newly crate.
+//   And use it both rmm and realm
+const RSI_HOST_CALL: usize = 0xC400_0199;
+
+#[repr(C)]
+pub struct HostCall {
+    pub imm: u16,
+    pub padding: u16,
+}
+
+static mut HOST_CALL: HostCall = HostCall { imm: 0, padding: 0 };
+
+unsafe fn smc(cmd: usize, arg: [usize; 4]) -> [usize; 8] {
+    let mut ret: [usize; 8] = [0usize; 8];
+    asm! {
+        "smc #0x0",
+        inlateout("x0") cmd => ret[0],
+        inlateout("x1") arg[0] => ret[1],
+        inlateout("x2") arg[1] => ret[2],
+        inlateout("x3") arg[2] => ret[3],
+        inlateout("x4") arg[3] => ret[4],
+        out("x5") ret[5],
+        out("x6") ret[6],
+        out("x7") ret[7],
     }
+    ret
 }

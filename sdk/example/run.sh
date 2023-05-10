2a002ce715f508624fe911cfb534dfafc05398f9
Sangwan Kwon <sangwan.kwon@samsung.com>
2023-05-10 11:21:18 +0900

    ci: Add sdk build & test
    Signed-off-by: Sangwan Kwon <sangwan.kwon@samsung.com>

--- sdk/example/run.sh
+++ sdk/example/run.sh
@@ -7,6 +7,6 @@ cargo build
 cd -
 g++ attestation.cpp \
 	-lislet_sdk \
-	-L/data/islet/out/x86_64-unknown-linux-gnu/debug/ \
+	-L ../../out/x86_64-unknown-linux-gnu/debug/ \
 	-I ../include
-LD_LIBRARY_PATH=/data/islet/out/x86_64-unknown-linux-gnu/debug/ ./a.out
+LD_LIBRARY_PATH=../../out/x86_64-unknown-linux-gnu/debug/ ./a.out

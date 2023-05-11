e7b9a5903e4aa8a4a0a27702cb82903c11960ffc
Jinbum Park <jinb.park@samsung.com>
2023-05-11 09:02:31 +0900

    move prebuilt certifier service into islet-asset
    in order to leave no executables in islet repo
    
    Signed-off-by: Jinbum Park <jinb.park@samsung.com>

--- examples/confidential-ml/certifier-service/clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-rm -f *.bin
-rm -f *.measurement
-rm -f *.cc
-rm -f policy_store
--- examples/confidential-ml/certifier-service/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-ARCH=$1
-cp -f ../certifier-data/* .
-./simpleserver.${ARCH} --policyFile=policy.bin --readPolicy=true --host="0.0.0.0"

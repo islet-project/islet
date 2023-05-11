eb53d4e0db71bb1df2154efdaa2b9e8476e908d5
Sangwan Kwon <sangwan.kwon@samsung.com>
2023-05-11 13:56:23 +0900

    sdk: Add c_api documentation & README
    Signed-off-by: Sangwan Kwon <sangwan.kwon@samsung.com>

--- sdk/example/run.sh
+++ sdk/example/run.sh
@@ -7,12 +7,10 @@ cargo build
 cd -
 g++ attestation.cpp \
 	-lislet_sdk \
-	-L ../../out/x86_64-unknown-linux-gnu/debug/ \
-	-I ../include
+	-L ../../out/x86_64-unknown-linux-gnu/debug/
 LD_LIBRARY_PATH=../../out/x86_64-unknown-linux-gnu/debug/ ./a.out
 
 g++ sealing.cpp \
 	-lislet_sdk \
-	-L ../../out/x86_64-unknown-linux-gnu/debug/ \
-	-I ../include
+	-L ../../out/x86_64-unknown-linux-gnu/debug/
 LD_LIBRARY_PATH=../../out/x86_64-unknown-linux-gnu/debug/ ./a.out

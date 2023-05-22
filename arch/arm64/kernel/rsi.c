111757d7d6458e5dd669e32076359bb4513a02e1
Lukasz Pawelczyk <l.pawelczyk@samsung.com>
2023-05-22 18:02:34 +0200

    Add static variables for RSI in kernel address space
    They are required for linux-rsi driver working as out of tree module.

--- arch/arm64/kernel/rsi.c
+++ arch/arm64/kernel/rsi.c
@@ -11,6 +11,10 @@
 #include <asm/rsi.h>
 
 struct realm_config __attribute((aligned(PAGE_SIZE))) config;
+EXPORT_SYMBOL(config);
+
+char __attribute__((aligned(PAGE_SIZE))) rsi_page_buf[PAGE_SIZE];
+EXPORT_SYMBOL(rsi_page_buf);
 
 unsigned long prot_ns_shared;
 EXPORT_SYMBOL(prot_ns_shared);

8566b278196e12372b23cb7fb2990f045402c889
Sangwan Kwon <sangwan.kwon@samsung.com>
2023-05-24 10:27:44 +0900

    lib: Detach rsi-el0 library from rsictl
    Signed-off-by: Sangwan Kwon <sangwan.kwon@samsung.com>

--- lib/rsi-el0/src/lib.rs
+++ lib/rsi-el0/src/lib.rs
@@ -1,33 +1,34 @@
-pub(super) mod kernel;
+#![warn(rust_2018_idioms)]
+#![deny(warnings)]
+
+pub(crate) mod kernel;
 
 use nix::{fcntl::OFlag, libc::O_RDWR, sys::stat::Mode};
+
+pub const MAX_MEASUR_LEN: u16 = 0x40;
+pub const CHALLENGE_LEN: u16 = 0x40;
+pub const MAX_TOKEN_LEN: u16 = 0x1000;
 
 const FLAGS: OFlag = OFlag::from_bits_truncate(O_RDWR);
 const MODE: Mode = Mode::from_bits_truncate(0o644);
 const DEV: &str = "/dev/rsi";
 
-struct Fd
-{
+struct Fd {
     fd: i32,
 }
 
-impl Fd
-{
-    fn wrap(fd: i32) -> Self
-    {
+impl Fd {
+    fn wrap(fd: i32) -> Self {
         Self { fd }
     }
 
-    fn get(&self) -> i32
-    {
+    fn get(&self) -> i32 {
         self.fd
     }
 }
 
-impl Drop for Fd
-{
-    fn drop(&mut self)
-    {
+impl Drop for Fd {
+    fn drop(&mut self) {
         match nix::unistd::close(self.fd) {
             Ok(()) => (),
             Err(e) => println!("WARNING: close failed: {}", e),
@@ -35,8 +36,7 @@ impl Drop for Fd
     }
 }
 
-pub fn abi_version() -> nix::Result<(u32, u32)>
-{
+pub fn abi_version() -> nix::Result<(u32, u32)> {
     let fd = Fd::wrap(nix::fcntl::open("/dev/rsi", FLAGS, MODE)?);
     let mut version = 0;
     kernel::abi_version(fd.get(), &mut version)?;
@@ -46,23 +46,20 @@ pub fn abi_version() -> nix::Result<(u32, u32)>
     ))
 }
 
-pub fn measurement_read(index: u32) -> nix::Result<Vec<u8>>
-{
+pub fn measurement_read(index: u32) -> nix::Result<Vec<u8>> {
     let mut measure = [kernel::RsiMeasurement::new_empty(index)];
     let fd = Fd::wrap(nix::fcntl::open(DEV, FLAGS, MODE)?);
     kernel::measurement_read(fd.get(), &mut measure)?;
     Ok(measure[0].data[..(measure[0].data_len as usize)].to_vec())
 }
 
-pub fn measurement_extend(index: u32, data: &[u8]) -> nix::Result<()>
-{
+pub fn measurement_extend(index: u32, data: &[u8]) -> nix::Result<()> {
     let measur = [kernel::RsiMeasurement::new_from_data(index, data)];
     let fd = Fd::wrap(nix::fcntl::open(DEV, FLAGS, MODE)?);
     kernel::measurement_extend(fd.get(), &measur)
 }
 
-pub fn attestation_token(challenge: &[u8; 64]) -> nix::Result<Vec<u8>>
-{
+pub fn attestation_token(challenge: &[u8; 64]) -> nix::Result<Vec<u8>> {
     let mut attest = [kernel::RsiAttestation::new(challenge)];
     let fd = Fd::wrap(nix::fcntl::open(DEV, FLAGS, MODE)?);
     kernel::attestation_token(fd.get(), &mut attest)?;
--- realm/linux-rsi/rsictl/src/lib.rs
+++ realm/linux-rsi/rsictl/src/lib.rs
@@ -1,14 +1,13 @@
-mod ioctl;
 mod token;
 
-pub use ioctl::kernel::CHALLENGE_LEN;
-pub use ioctl::kernel::MAX_MEASUR_LEN;
-pub use ioctl::kernel::MAX_TOKEN_LEN;
+pub use rsi_el0::CHALLENGE_LEN;
+pub use rsi_el0::MAX_MEASUR_LEN;
+pub use rsi_el0::MAX_TOKEN_LEN;
 
-pub use ioctl::abi_version;
-pub use ioctl::attestation_token;
-pub use ioctl::measurement_extend;
-pub use ioctl::measurement_read;
+pub use rsi_el0::abi_version;
+pub use rsi_el0::attestation_token;
+pub use rsi_el0::measurement_extend;
+pub use rsi_el0::measurement_read;
 
 pub use token::dumper::print_token;
 pub use token::verifier::verify_token;

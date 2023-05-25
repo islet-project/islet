a15fb4a8ba248f9f4cc137fcfbeafb56c02d8147
Sangwan Kwon <sangwan.kwon@samsung.com>
2023-05-25 14:58:34 +0900

    assets: Change default rootfs for realms
    Signed-off-by: Sangwan Kwon <sangwan.kwon@samsung.com>

--- scripts/fvp/launch-realm.sh
+++ scripts/fvp/launch-realm.sh
@@ -7,9 +7,10 @@
 	--disable-sve \
 	--console serial \
 	--irqchip=gicv3 \
+	--network mode=user \
 	-m 256M \
 	-c 1 \
 	-k linux.realm \
-	-i rootfs-realm.cpio.gz \
+	-i rootfs-realm-examples.cpio.gz \
 	-p "earlycon=ttyS0 printk.devkmsg=on" \
 	--9p /shared,FMR

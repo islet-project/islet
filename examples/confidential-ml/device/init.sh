fa851c58aeb855d9001ab4f4891400f7417c9204
Jinbum Park <jinb.park@samsung.com>
2023-05-26 10:28:28 +0900

    support code generation model in runtime and device
    Signed-off-by: Jinbum Park <jinb.park@samsung.com>

--- examples/confidential-ml/device/init.sh
+++ examples/confidential-ml/device/init.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 
-export LD_LIBRARY_PATH=/lib
-
 rm -rf data
 mkdir data
 cp -f ../certifier-data/* data/
 
-date -s "2023-09-18 00:00:00"  # [hack to skip time check]
+HOST="0.0.0.0"
+if [ "$1" ]; then
+  HOST=$1
+fi
 
-ln -s /shared/examples/lib/libtensorflowlite.so /lib/libtensorflowlite.so
-ln -s /shared/examples/lib/libtensorflowlite_flex.so /lib/libtensorflowlite_flex.so
-
-HOST=$1
 ./device.exe --print_all=true \
       --operation=cold-init --data_dir=./data/ --measurement_file="example_app.measurement" \
       --policy_store_file=policy_store --policy_host="${HOST}"

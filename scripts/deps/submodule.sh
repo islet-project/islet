24608b6b2841a5e94ae8cdc509f0f4b1bbef24f7
Sangwan Kwon <sangwan.kwon@samsung.com>
2023-06-15 17:05:31 +0900

    ci: Replace worktree to submodule
    Signed-off-by: Sangwan Kwon <sangwan.kwon@samsung.com>

--- scripts/deps/submodule.sh
+++ scripts/deps/submodule.sh
@@ -3,9 +3,5 @@
 set -e
 
 ROOT=$(git rev-parse --show-toplevel)
-THIRD_PARTY=$ROOT/third-party
-TF_RMM=$THIRD_PARTY/tf-rmm
-KVM_UNIT_TESTS=$THIRD_PARTY/kvm-unit-tests
 
-cd $TF_RMM && git submodule update --init
-cd $KVM_UNIT_TESTS && git submodule update --init
+cd $ROOT && git submodule update --init --recursive

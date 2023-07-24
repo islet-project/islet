845b7a0ce12cf9cc34f94e63793cbf8c0d26fb58
Changho Choi <ch754.choi@samsung.com>
2023-07-24 10:51:10 +0900

    script: Print the realm's log in uart1

--- scripts/fvp/launch-realm-islet.sh
+++ scripts/fvp/launch-realm-islet.sh
@@ -10,9 +10,9 @@
 	--console serial \
 	--irqchip=gicv3 \
 	--network virtio \
+	--9p /shared,FMR \
 	-m 256M \
 	-c 1 \
 	-k linux.realm \
 	-i rootfs-realm.cpio.gz \
-	-p "earlycon=ttyS0 printk.devkmsg=on" \
-	--9p /shared,FMR
+	-p "console=pl011,mmio,0x1c0a0000 console=ttyAMA0 printk.devkmsg=on"
---
+++ scripts/fvp/launch-realm-tf-rmm.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+./configure-net.sh &
+
+./lkvm run \
+	--debug \
+	--realm \
+	--measurement-algo="sha256" \
+	--disable-sve \
+	--console serial \
+	--irqchip=gicv3 \
+	--network virtio \
+	--9p /shared,FMR \
+	-m 256M \
+	-c 1 \
+	-k linux.realm \
+	-i rootfs-realm.cpio.gz \
+	-p "earlycon=ttyS0 printk.devkmsg=on"

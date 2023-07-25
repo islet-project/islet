e8564d0cb3b1236aea7118baf440990b45d195c2
Sangwan Kwon <sangwan.kwon@samsung.com>
2023-07-25 10:21:28 +0900

    rmi: Fix GRANULE_UN/DELEGATE's return according to spec
    Signed-off-by: Sangwan Kwon <sangwan.kwon@samsung.com>

--- rmm/monitor/src/rmi/gpt.rs
+++ rmm/monitor/src/rmi/gpt.rs
@@ -4,62 +4,43 @@ use crate::rmi;
 use crate::rmi::error::Error;
 use crate::rmm::granule;
 use crate::rmm::granule::GranuleState;
-use crate::rmm::PageMap;
 use crate::smc;
 extern crate alloc;
 
 pub fn set_event_handler(mainloop: &mut Mainloop) {
-    listen!(mainloop, rmi::GRANULE_DELEGATE, |arg, ret, rmm| {
-        let smc = rmm.smc;
-        let mm = rmm.mm;
-        mark_realm(smc, mm, arg[0], ret)?;
-        Ok(())
+    listen!(mainloop, rmi::GRANULE_DELEGATE, |arg, _, rmm| {
+        mark_realm(rmm.smc, arg[0])
     });
 
-    listen!(mainloop, rmi::GRANULE_UNDELEGATE, |arg, ret, rmm| {
-        let smc = rmm.smc;
-        let mm = rmm.mm;
-        mark_ns(smc, mm, arg[0], ret)?;
-        Ok(())
+    listen!(mainloop, rmi::GRANULE_UNDELEGATE, |arg, _, rmm| {
+        mark_ns(rmm.smc, arg[0])
     });
 }
 
-pub fn mark_realm(
-    smc: smc::SecureMonitorCall,
-    _mm: PageMap,
-    addr: usize,
-    ret: &mut [usize],
-) -> Result<(), Error> {
+pub fn mark_realm(smc: smc::SecureMonitorCall, addr: usize) -> Result<(), Error> {
     let cmd = smc.convert(smc::Code::MarkRealm);
 
     if granule::set_granule(addr, GranuleState::Delegated) != granule::RET_SUCCESS {
-        //ret[1] = addr; // [JB] ??
         return Err(Error::RmiErrorInput);
-    } else {
-        let smc_ret = smc.call(cmd, &[addr]);
-        // XXX: Should we use ret[0] for smc return value?
-        //      For RMI calls, ret[0] is mostly used for RMI's result value
-        ret[0] = smc_ret[0];
     }
+
+    if smc.call(cmd, &[addr])[0] != smc::SMC_SUCCESS {
+        return Err(Error::RmiErrorInput);
+    }
+
     Ok(())
 }
 
-pub fn mark_ns(
-    smc: smc::SecureMonitorCall,
-    _mm: PageMap,
-    addr: usize,
-    ret: &mut [usize],
-) -> Result<(), Error> {
+pub fn mark_ns(smc: smc::SecureMonitorCall, addr: usize) -> Result<(), Error> {
     let cmd = smc.convert(smc::Code::MarkNonSecure);
 
     if granule::set_granule(addr, GranuleState::Undelegated) != granule::RET_SUCCESS {
-        // ret[1] = addr;  // [JB] GRANULE_DELEGATE & GRANULE_UNDELEGATE have only one output value
         return Err(Error::RmiErrorInput);
-    } else {
-        let smc_ret = smc.call(cmd, &[addr]);
-        // XXX: Should we use ret[0] for smc return value?
-        //      For RMI calls, ret[0] is mostly used for RMI's result value
-        ret[0] = smc_ret[0];
     }
+
+    if smc.call(cmd, &[addr])[0] != smc::SMC_SUCCESS {
+        panic!("A delegated granule should only be undelegated on request from RMM.");
+    }
+
     Ok(())
 }

e81b6347b431713cb6babd0154765a3e5f104a4c
Sangwan Kwon <sangwan.kwon@samsung.com>
2023-08-10 13:40:05 +0900

    Print structures more pretty
    Signed-off-by: Sangwan Kwon <sangwan.kwon@samsung.com>

--- rmm/monitor/src/rmi/realm/params.rs
+++ rmm/monitor/src/rmi/realm/params.rs
@@ -2,7 +2,6 @@ use crate::host::Accessor as HostAccessor;
 use crate::rmm::granule::GRANULE_SIZE;
 
 #[repr(C)]
-#[derive(Debug)]
 pub struct Params {
     pub features_0: u64,
     padding0: [u8; 248],
@@ -12,11 +11,13 @@ pub struct Params {
     padding2: [u8; 960],
     pub vmid: u16,
     padding3: [u8; 6],
-    pub rtt_addr: u64,
+    pub rtt_base: u64,
     pub rtt_level_start: i64,
     pub rtt_num_start: u32,
     padding4: [u8; 2020],
 }
+
+const_assert_eq!(core::mem::size_of::<Params>(), GRANULE_SIZE);
 
 impl Default for Params {
     fn default() -> Self {
@@ -29,7 +30,7 @@ impl Default for Params {
             padding2: [0; 960],
             vmid: 0,
             padding3: [0; 6],
-            rtt_addr: 0,
+            rtt_base: 0,
             rtt_level_start: 0,
             rtt_num_start: 0,
             padding4: [0; 2020],
@@ -37,7 +38,19 @@ impl Default for Params {
     }
 }
 
-const_assert_eq!(core::mem::size_of::<Params>(), GRANULE_SIZE);
+impl core::fmt::Debug for Params {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        f.debug_struct("Params")
+            .field("features_0", &format_args!("{:#X}", &self.features_0))
+            .field("hash_algo", &self.hash_algo)
+            .field("rpv", &self.rpv)
+            .field("vmid", &self.vmid)
+            .field("rtt_base", &format_args!("{:#X}", &self.rtt_base))
+            .field("rtt_level_start", &self.rtt_level_start)
+            .field("rtt_num_start", &self.rtt_num_start)
+            .finish()
+    }
+}
 
 impl HostAccessor for Params {}
 
@@ -53,7 +66,7 @@ pub mod test {
         assert_eq!(offset_of!(Params, hash_algo), 0x100);
         assert_eq!(offset_of!(Params, rpv), 0x400);
         assert_eq!(offset_of!(Params, vmid), 0x800);
-        assert_eq!(offset_of!(Params, rtt_addr), 0x808);
+        assert_eq!(offset_of!(Params, rtt_base), 0x808);
         assert_eq!(offset_of!(Params, rtt_level_start), 0x810);
         assert_eq!(offset_of!(Params, rtt_num_start), 0x818);
     }

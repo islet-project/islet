f2f615dceef60df9fbd882bddb82191c52bff593
Sangwan Kwon <sangwan.kwon@samsung.com>
2023-08-10 13:00:06 +0900

    spec: Refine RmiRealmParams
    Signed-off-by: Sangwan Kwon <sangwan.kwon@samsung.com>

--- rmm/monitor/src/rmi/realm/params.rs
+++ rmm/monitor/src/rmi/realm/params.rs
@@ -1,87 +1,67 @@
 use crate::host::Accessor as HostAccessor;
 
 #[repr(C)]
+#[derive(Debug)]
 pub struct Params {
-    features0: Features0,
-    hash_algo: HashAlgo,
-    rpv: RPV,
-    inner: Inner,
+    pub features_0: u64,
+    padding0: [u8; 248],
+    pub hash_algo: u8,
+    padding1: [u8; 767],
+    pub rpv: [u8; 64],
+    padding2: [u8; 960],
+    pub vmid: u16,
+    padding3: [u8; 6],
+    pub rtt_addr: u64,
+    pub rtt_level_start: i64,
+    pub rtt_num_start: u32,
+    padding4: [u8; 2020],
 }
 
 impl Default for Params {
     fn default() -> Self {
         Self {
-            features0: Features0 { val: 0 },
-            hash_algo: HashAlgo { val: 0 },
-            rpv: RPV { val: [0; RPV_SIZE] },
-            inner: Inner {
-                val: core::mem::ManuallyDrop::new(_Inner {
-                    vmid: 0,
-                    rtt_base: 0,
-                    rtt_level_start: 0,
-                    rtt_num_start: 0,
-                }),
-            },
-        }
-    }
-}
-
-impl Drop for Params {
-    fn drop(&mut self) {
-        unsafe {
-            core::mem::ManuallyDrop::drop(&mut self.inner.val);
+            features_0: 0,
+            padding0: [0; 248],
+            hash_algo: 0,
+            padding1: [0; 767],
+            rpv: [0; 64],
+            padding2: [0; 960],
+            vmid: 0,
+            padding3: [0; 6],
+            rtt_addr: 0,
+            rtt_level_start: 0,
+            rtt_num_start: 0,
+            padding4: [0; 2020],
         }
     }
 }
 
 impl HostAccessor for Params {}
 
-impl core::fmt::Debug for Params {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        // Safety: union type should be initialized
-        unsafe {
-            f.debug_struct("Params")
-                .field("features0", &format_args!("{:#X}", &self.features0.val))
-                .field("hash_algo", &self.hash_algo.val)
-                .field("rpv", &self.rpv.val)
-                .field("vmid", &self.inner.val.vmid)
-                .field("rtt_base", &format_args!("{:#X}", &self.inner.val.rtt_base))
-                .field("rtt_level_start", &self.inner.val.rtt_level_start)
-                .field("rtt_num_start", &self.inner.val.rtt_num_start)
-                .finish()
-        }
+#[cfg(test)]
+pub mod test {
+    use super::*;
+
+    macro_rules! offset_of {
+        ($type:ty, $field:tt) => {{
+            let dummy = core::mem::MaybeUninit::<$type>::uninit();
+            let dummy_ptr = dummy.as_ptr();
+            let member_ptr = unsafe { ::core::ptr::addr_of!((*dummy_ptr).$field) };
+
+            member_ptr as usize - dummy_ptr as usize
+        }};
     }
-}
-#[repr(C)]
-union Features0 {
-    val: u64,
-    reserved: [u8; 0x100],
-}
 
-#[repr(C)]
-union HashAlgo {
-    val: u8,
-    reserved: [u8; 0x400 - 0x100],
-}
+    #[test]
+    fn spec_params() {
+        assert_eq!(core::mem::size_of::<Params>(), 4096);
 
-const RPV_SIZE: usize = 64;
-#[repr(C)]
-union RPV {
-    // Realm Personalization Value
-    val: [u8; RPV_SIZE],
-    reserved: [u8; 0x800 - 0x400],
-}
-
-#[repr(C)]
-struct _Inner {
-    vmid: u16,
-    rtt_base: u64,
-    rtt_level_start: i64,
-    rtt_num_start: u32,
-}
-
-#[repr(C)]
-union Inner {
-    val: core::mem::ManuallyDrop<_Inner>,
-    reserved: [u8; 0x1000 - 0x800],
+        assert_eq!(offset_of!(Params, features_0), 0x0);
+        assert_eq!(offset_of!(Params, hash_algo), 0x100);
+        assert_eq!(offset_of!(Params, rpv), 0x400);
+        assert_eq!(offset_of!(Params, vmid), 0x800);
+        assert_eq!(offset_of!(Params, rtt_addr), 0x808);
+        assert_eq!(offset_of!(Params, rtt_level_start), 0x810);
+        assert_eq!(offset_of!(Params, rtt_num_start), 0x818);
+    }
 }

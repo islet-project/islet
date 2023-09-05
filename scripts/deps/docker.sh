a7a798d455ee100e7c9806e8b05eeb98235ca9ee
Sangwan Kwon <sangwan.kwon@samsung.com>
2023-09-05 11:15:34 +0900

    ci: Fix crates test to aarch64
    Signed-off-by: Sangwan Kwon <sangwan.kwon@samsung.com>

--- scripts/deps/docker.sh
+++ scripts/deps/docker.sh
@@ -10,9 +10,9 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
 echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+	"deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+	"$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" \
+	| sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 
 sudo apt-get update
 

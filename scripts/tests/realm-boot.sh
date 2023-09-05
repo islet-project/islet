4d86c14d101e0bab453bd66d582f4f18b017e73b
Changho Choi <ch754.choi@samsung.com>
2023-09-05 16:59:57 +0900

    ci: Add a realm-linux booting test

---
+++ scripts/tests/realm-boot.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+ROOT=$(git rev-parse --show-toplevel)
+UART=$ROOT/out/uart0.log
+
+[ -e $UART ] && rm $UART
+
+check_result()
+{
+	# cleanup
+	ps -ef | grep fvp-cca | grep -v grep | awk '{print $2}' | xargs kill
+	ps -ef | grep "FVP terminal" | grep -v grep | awk '{print $2}' | xargs kill
+	ps -ef | grep FVP_Base_RevC-2xAEMvA | grep -v grep | awk '{print $2}' | xargs kill
+
+	# report
+	LOGIN=$(tail -30 $UART | grep "login" | awk '{print $2}')
+	LOGIN="${LOGIN//[$'\t\r\n ']/}"
+
+	if [ "$LOGIN" == "" ]; then
+		echo "[-] Test result: Realm booting failed with the following log"
+		tail -30 $UART
+		echo "Try increasing the time for sleep"
+		exit 1
+	fi
+
+	echo "[!] Tests result: Realm booting succeeded"
+}
+
+$ROOT/scripts/fvp-cca -bo -nw=linux --realm=linux -rmm=islet --realm-launch
+$ROOT/scripts/fvp-cca -ro -nw=linux --realm=linux -rmm=islet --realm-launch &
+
+sleep 750
+
+check_result

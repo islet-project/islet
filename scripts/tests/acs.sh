e072e573328c582d0c42ce69c28b97f381e82145
Sangwan Kwon <sangwan.kwon@samsung.com>
2023-09-07 10:40:40 +0900

    scripts: Update ACS test
    Signed-off-by: Sangwan Kwon <sangwan.kwon@samsung.com>

--- scripts/tests/acs.sh
+++ scripts/tests/acs.sh
@@ -2,27 +2,44 @@
 
 set -e
 
+# Control these variables
+EXPECTED=11
+TIMEOUT=1000
+
 ROOT=$(git rev-parse --show-toplevel)
-REPORT=$ROOT/third-party/cca-rmm-acs/build/output/regression_report.log
+UART=$ROOT/out/uart2.log
 
-$ROOT/scripts/fvp-cca -bo -nw=acs -rmm=tf-rmm
-$ROOT/scripts/fvp-cca -ro -nw=acs -rmm=tf-rmm &
+[ -e $UART ] && rm $UART
 
-flag=0
-done=1
-echo -n "Running ACS test"
+$ROOT/scripts/fvp-cca -bo -nw=acs
+$ROOT/scripts/fvp-cca -ro -nw=acs --no-telnet &
 
-sleep 10
+sleep 30
+elapsed=30
 
-while ! grep -q "REGRESSION REPORT:" "$REPORT"; do
-	sleep 10
+while ! grep -q "REGRESSION REPORT:" "$UART"; do
+	sleep 5
+	elapsed=$((elapsed + 5))
+
+	if [ ${elapsed} -gt ${TIMEOUT} ]; then
+		echo "[!] Timeout occured."
+		break
+	fi
+
 	echo -n "."
 done
 
-tail -11 $REPORT
-
-# cleanup
+# Cleanup
 ps -ef | grep fvp-cca | grep -v grep | awk '{print $2}' | xargs kill
 ps -ef | grep "FVP terminal" | grep -v grep | awk '{print $2}' | xargs kill
+ps -ef | grep FVP_Base_RevC-2xAEMvA | grep -v grep | awk '{print $2}' | xargs kill
 
-cp $REPORT $ROOT/out
+tail -11 $UART
+passed=$(tail -11 $UART | grep "TOTAL PASSED" | awk '{print $5}')
+
+if [ "$EXPECTED" -eq "$passed" ]; then
+	echo "[!] Test succeeded!"
+else
+	echo "[!] Test failed!"
+	exit 1
+fi

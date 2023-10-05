9756caadc2b783d30955e1b895ffaf03eacf5016
Changho Choi <ch754.choi@samsung.com>
2023-10-05 20:00:37 +0900

    ci: Fix the issue regarding not found
    This commit fixes the below issue.
    
    ```
    Run ./scripts/init_min.sh
    ...
    0 upgraded, 65 newly installed, 0 to remove and 30 not upgraded.
    Need to get 54.1 MB of archives.
    After this operation, 220 MB of additional disk space will be used.
    E: Failed to fetch http://security.ubuntu.com/ubuntu/pool/main/g/glibc/libc6-dev-i386_2.35-0ubuntu3.3_amd64.deb  404  Not Found [IP: 40.81.13.82 80]
    E: Failed to fetch http://security.ubuntu.com/ubuntu/pool/main/g/glibc/libc6-x32_2.35-0ubuntu3.3_amd64.deb  404  Not Found [IP: 40.81.13.82 80]
    E: Failed to fetch http://security.ubuntu.com/ubuntu/pool/main/g/glibc/libc6-dev-x32_2.35-0ubuntu3.3_amd64.deb  404  Not Found [IP: 40.81.13.82 80]
    E: Unable to fetch some archives, maybe run apt-get update or try with --fix-missing?
    Error: Process completed with exit code 100.
    ```

---
+++ scripts/deps/pkgs-for-ci.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+sudo apt-get update
+
+sudo apt install -y -qq --no-install-recommends \
+	gcc-multilib cmake libssl-dev \
+	binutils python3-pip \
+	device-tree-compiler xterm fakeroot mtools fdisk cpio \
+	dosfstools e2fsprogs \
+	libxml-libxml-perl \
+	jq lcov graphviz inkscape \
+	flex bison \
+	bzip2 \
+	srecord
+
+pip3 install toml

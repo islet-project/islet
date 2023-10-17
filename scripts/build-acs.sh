f688dcab69d3094f6de8264e7665335140d09cf8
Changho Choi <ch754.choi@samsung.com>
2023-10-17 20:17:01 +0900

    script: Add an option to select ACS's test to run
    Usage 1) Run a single ACS test named `test_name`
    
    ./scripts/fvp-cca -nw=acs --selected-tests=test_name
    
    e.g.) ./scripts/fvp-cca -nw=acs --selected-tests=cmd_granule_delegate
    
    Usage 2) Run multiple ACS tests starting from test_name1 to test_name2
    
    ./scripts/fvp-cca -nw=acs --selected-tests="test_name1;test_name2"
    
    e.g.) ./scripts/fvp-cca -nw=acs --selected-tests="cmd_granule_delegate;exception_rec_exit_ia"

--- scripts/build-acs.sh
+++ scripts/build-acs.sh
@@ -8,5 +8,10 @@ CROSS_COMPILE=$ROOT/assets/toolchain/aarch64-none-elf/bin/aarch64-none-elf-
 cd $ROOT/third-party/cca-rmm-acs
 mkdir -p build && cd build
 
-cmake ../ -G"Unix Makefiles" -DCROSS_COMPILE=$CROSS_COMPILE -DTARGET=tgt_tfa_fvp -DTEST_COMBINE=ON -DSREC_CAT=/bin/srec_cat
+if [ $# -eq 0 ]; then
+	cmake ../ -G"Unix Makefiles" -DCROSS_COMPILE=$CROSS_COMPILE -DTARGET=tgt_tfa_fvp -DTEST_COMBINE=ON -DSREC_CAT=/bin/srec_cat
+elif [ $# -eq 2 ]; then
+	SELECTED_RANGE="$1;$2"
+	cmake ../ -G"Unix Makefiles" -DCROSS_COMPILE=$CROSS_COMPILE -DTARGET=tgt_tfa_fvp -DTEST_COMBINE=ON -DSREC_CAT=/bin/srec_cat -DSUITE_TEST_RANGE=$SELECTED_RANGE
+fi
 make

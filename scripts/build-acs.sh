a22637e6a0a15d5486cef7d9fa89080758c54505
Changho Choi <ch754.choi@samsung.com>
2023-10-18 18:13:45 +0900

    script: Add an option to exclude ACS's tests
    Usage 1) Run all ACS tests except user-excluded tests
    
    ./scripts/fvp-cca -nw=acs --excluded-tests=file_name
    
    e.g.) ./scripts/fvp-cca -nw=acs --excluded-tests=skip.txt
    
    Usage 2) Run multiple ACS tests starting from test_name1 to test_name2
             except user-excluded tests
    
    ./scripts/fvp-cca -nw=acs --excluded-tests=file_name --selected-tests="test_name1;test_name2"
    
    e.g.) ./scripts/fvp-cca -nw=acs --excluded-tests=test.txt --selected-tests="cmd_granule_delegate;cmd_realm_create"

--- scripts/build-acs.sh
+++ scripts/build-acs.sh
@@ -10,8 +10,15 @@ mkdir -p build && cd build
 
 if [ $# -eq 0 ]; then
 	cmake ../ -G"Unix Makefiles" -DCROSS_COMPILE=$CROSS_COMPILE -DTARGET=tgt_tfa_fvp -DTEST_COMBINE=ON -DSREC_CAT=/bin/srec_cat
+elif [ $# -eq 1 ]; then
+	EXCLUDED_LIST="$1"
+	cmake ../ -G"Unix Makefiles" -DCROSS_COMPILE=$CROSS_COMPILE -DTARGET=tgt_tfa_fvp -DTEST_COMBINE=ON -DSREC_CAT=/bin/srec_cat -DSUITE_EXCLUDED=$EXCLUDED_LIST
 elif [ $# -eq 2 ]; then
 	SELECTED_RANGE="$1;$2"
 	cmake ../ -G"Unix Makefiles" -DCROSS_COMPILE=$CROSS_COMPILE -DTARGET=tgt_tfa_fvp -DTEST_COMBINE=ON -DSREC_CAT=/bin/srec_cat -DSUITE_TEST_RANGE=$SELECTED_RANGE
+elif [ $# -eq 3 ]; then
+	EXCLUDED_LIST="$1"
+	SELECTED_RANGE="$2;$3"
+	cmake ../ -G"Unix Makefiles" -DCROSS_COMPILE=$CROSS_COMPILE -DTARGET=tgt_tfa_fvp -DTEST_COMBINE=ON -DSREC_CAT=/bin/srec_cat -DSUITE_EXCLUDED=$EXCLUDED_LIST -DSUITE_TEST_RANGE=$SELECTED_RANGE
 fi
 make

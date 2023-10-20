bb9fd0aefe1d1d3b4e5a5c73c3895e7bcd1d49c3
Changho Choi <ch754.choi@samsung.com>
2023-10-20 17:24:27 +0900

    ci: Use --rmm-log-level=error to avoid a storage issue
    [before]
    ```
    ...
    [!] Setting islet features: ['--features', 'max_level_trace']
    [!] Building realm management monitor...: islet
    [!] Building tf-a-tests...
    [!] Building bootloaders(bl1.bin, fip.bin)...
    [-] Test failed! (There are no proper result logs)
    Error: Process completed with exit code 1.
    ```
    
    [after]
    ```
    ...
    [!] Setting islet features: ['--features', 'max_level_error']
    [!] Building realm management monitor...: islet
    [!] Building tf-a-tests...
    [!] Building bootloaders(bl1.bin, fip.bin)...
    [!] Tests result: 6 passed, 0 failed.
    [!] Setting islet features: ['--features', 'max_level_error']
    [!] Building realm management monitor...: islet
    [!] Building realm(rsi-test)...
    [!] Building tf-a-tests...
    [!] Building bootloaders(bl1.bin, fip.bin)...
    [!] Tests result: 6 passed, 0 failed.
    ```

--- scripts/tests/tf-a-tests.sh
+++ scripts/tests/tf-a-tests.sh
@@ -47,7 +47,7 @@ sleep 20
 check_result
 
 # islet-rmm tests
-$ROOT/scripts/fvp-cca -bo -nw=tf-a-tests -rmm=islet
+$ROOT/scripts/fvp-cca -bo -nw=tf-a-tests -rmm=islet --rmm-log-level=error
 $ROOT/scripts/fvp-cca -ro -nw=tf-a-tests -rmm=islet &
 
 sleep 20
@@ -55,7 +55,7 @@ sleep 20
 check_result
 
 # islet-rmm tests with rsi-test realm
-$ROOT/scripts/fvp-cca -bo -nw=tf-a-tests -rmm=islet -rm=rsi-test
+$ROOT/scripts/fvp-cca -bo -nw=tf-a-tests -rmm=islet -rm=rsi-test --rmm-log-level=error
 $ROOT/scripts/fvp-cca -ro -nw=tf-a-tests -rmm=islet -rm=rsi-test &
 
 sleep 20

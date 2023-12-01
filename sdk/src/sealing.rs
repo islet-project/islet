b67bc8c86bf3a64b3d9169cc71942aa0b373b798
Changho Choi <ch754.choi@samsung.com>
2023-12-01 11:55:15 +0900

    sdk: Use openssl instead of rsa to avoid a vulnerability
    This commit is for resolving the following error in ci.
    ```
    Run actions-rs/audit-check@v1
    /home/runner/.cargo/bin/cargo generate-lockfile
        Updating crates.io index
    Calling cargo-audit (JSON output)
    Warning: 1 vulnerabilities found!
    Warning: 1 warnings found!
    Found 1 advisory(ies), 1 other
    Warning: Unknown warning kind unsound found, please, file a bug
    Error: Critical vulnerabilities were found, marking check as failed
    ```
    
    The vulnerability (CVE-2023-49092) has been reported to be inside rsa crate.
    ```
    $ cargo audit
        Fetching advisory database from `https://github.com/RustSec/advisory-db.git`
          Loaded 580 security advisories (from /home/changho/.cargo/advisory-db)
        Updating crates.io index
        Scanning Cargo.lock for vulnerabilities (129 crate dependencies)
    Crate:     rsa
    Version:   0.9.5
    Title:     Marvin Attack: potential key recovery through timing sidechannels
    Date:      2023-11-22
    ID:        RUSTSEC-2023-0071
    URL:       https://rustsec.org/advisories/RUSTSEC-2023-0071
    Solution:  No fixed upgrade is available!
    Dependency tree:
    rsa 0.9.5
    └── islet_sdk 0.1.0
    
    error: 1 vulnerability found!
    ```

--- sdk/src/sealing.rs
+++ sdk/src/sealing.rs
@@ -1,25 +1,28 @@
 use crate::error::Error;
 
-use rand::rngs::ThreadRng;
-use rsa::pkcs1::DecodeRsaPrivateKey;
-use rsa::{Pkcs1v15Encrypt, RsaPrivateKey};
+use openssl::rsa::Padding;
+use openssl::rsa::Rsa;
 
 pub const DEBUG_KEY: &[u8] = include_bytes!("../debug/rsa2048-priv.der");
 
 pub fn seal(plaintext: &[u8]) -> Result<Vec<u8>, Error> {
-    let pri_key = RsaPrivateKey::from_pkcs1_der(DEBUG_KEY).or(Err(Error::SealingKey))?;
-    let pub_key = pri_key.to_public_key();
-    let mut rng: ThreadRng = Default::default();
-    let sealed = pub_key
-        .encrypt(&mut rng, Pkcs1v15Encrypt, &plaintext[..])
+    let pri_key = Rsa::private_key_from_der(DEBUG_KEY).or(Err(Error::SealingKey))?;
+    let padding = Padding::PKCS1;
+    let len = core::cmp::max(plaintext.len(), pri_key.size() as usize);
+    let mut sealed = vec![0 as u8; len];
+    let _ = pri_key
+        .public_encrypt(plaintext, &mut sealed, padding)
         .or(Err(Error::Sealing))?;
     Ok(sealed)
 }
 
 pub fn unseal(sealed: &[u8]) -> Result<Vec<u8>, Error> {
-    let pri_key = RsaPrivateKey::from_pkcs1_der(DEBUG_KEY).or(Err(Error::SealingKey))?;
-    let dec_plaintext = pri_key
-        .decrypt(Pkcs1v15Encrypt, &sealed)
-        .or(Err(Error::Sealing))?;
+    let pri_key = Rsa::private_key_from_der(DEBUG_KEY).or(Err(Error::SealingKey))?;
+    let padding = Padding::PKCS1;
+    let len = core::cmp::max(sealed.len(), pri_key.size() as usize);
+    let mut dec_plaintext = vec![0 as u8; len];
+    let _ = pri_key
+        .private_decrypt(sealed, &mut dec_plaintext, padding)
+        .or(Err(Error::SealingKey))?;
     Ok(dec_plaintext)
 }

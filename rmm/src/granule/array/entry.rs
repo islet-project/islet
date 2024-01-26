0d4a728787fa6bbbf69d0da951a2553030e37c8b
Changho Choi <ch754.choi@samsung.com>
2024-01-26 15:55:35 +0900

    Add an array-based granule status table
    This commit adds an array-based granule status table. The intention is
    for reducing the overall complexity of page table-based one, helping
    to reason about the implementation. The imposed memory overhead is
    approximately 2 MB == 2 bytes (ENTRY_SIZE) * 0xfc000 (RMM_MAX_GRANULES).
    To select the previous implementation, `gst_page_table` feature can be
    used.
    
    1. The Change in the Caller side
    ```
    [before]
    // Previously, the below match was used to expand the page table entry.
    let mut granule = match get_granule_if!(addr, GranuleState::Undelegated) {
         Err(MmError::MmNoEntry) => set_state_and_get_granule!(addr, GranuleState::Undelegated)
         other => other,
    }?;
    [after]
    // The match is no longer used, as the array will not be expanded.
    let mut granule = get_granule_if!(addr, GranuleState::Undelegated)?;
    ```
    
    2. Summary of Changes
    - Remove `set_state_and_get_granule` macro which is not necessary in array-based GST
    - Remove using `set_granule_raw()` function
    - Remove using `set_granule` in GranuleStatusTable
    - Remove using `set_granule_with_parent`
    - Remove using `Inner` and `Rc` in Entry (two less layers)
    - Remove using EntryGuard (one less layer)
    - Change slightly the handling of `is_not_in_realm()`
    - Replace using `vmsa::error::Error` with `crate::rmi::error::Error` (one less layer)
    - The end value change of FVP_DRAM0_REGION and FVP_DRAM1_REGION (plus one from the original value)
    - GranuleStatus's inner type change from u64 to u8
    
    Signed-off-by: Changho Choi <ch754.choi@samsung.com>

---
+++ rmm/src/granule/array/entry.rs
@@ -0,0 +1,87 @@
+use crate::granule::array::GRANULE_STATUS_TABLE;
+use crate::rmi::error::Error;
+
+use super::{GranuleState, GRANULE_SIZE};
+use spinning_top::{Spinlock, SpinlockGuard};
+use vmsa::guard::Content;
+
+use crate::granule::{FVP_DRAM0_REGION, FVP_DRAM1_IDX, FVP_DRAM1_REGION};
+
+// Safety: concurrency safety
+//  - For a granule status table that manages granules, it doesn't use a big lock for efficiency.
+//    So, we need to associate "lock" with each granule entry.
+
+pub struct Granule {
+    /// granule state
+    state: u8,
+}
+
+impl Granule {
+    pub fn state(&self) -> u8 {
+        self.state
+    }
+
+    pub fn set_state(&mut self, state: u8) -> Result<(), Error> {
+        if state == GranuleState::Delegated {
+            self.zeroize();
+        }
+        self.state = state;
+        Ok(())
+    }
+
+    pub fn content_mut<T: Content>(&mut self) -> &mut T {
+        let addr = self.index_to_addr();
+        unsafe { &mut *(addr as *mut T) }
+    }
+
+    pub fn content<T: Content>(&self) -> &T {
+        let addr = self.index_to_addr();
+        unsafe { &*(addr as *const T) }
+    }
+
+    fn index(&self) -> usize {
+        let entry_size = core::mem::size_of::<Entry>();
+        let granule_size = core::mem::size_of::<Granule>();
+        //  XXX: is there a clever way of getting the Entry from Granule (e.g., container_of())?
+        //  [        Entry        ]
+        //  [  offset ] [ Granule ]
+        let granule_offset = entry_size - granule_size;
+        let granule_addr = self as *const Granule as usize;
+        let entry_addr = granule_addr - granule_offset;
+        if let Some(gst) = unsafe { &mut GRANULE_STATUS_TABLE } {
+            let table_base = gst.entries.as_ptr() as usize;
+            (entry_addr - table_base) / core::mem::size_of::<Entry>()
+        } else {
+            unreachable!();
+        }
+    }
+
+    fn index_to_addr(&self) -> usize {
+        let idx = self.index();
+        if idx < FVP_DRAM1_IDX {
+            return FVP_DRAM0_REGION.start + (idx * GRANULE_SIZE);
+        }
+        FVP_DRAM1_REGION.start + ((idx - FVP_DRAM1_IDX) * GRANULE_SIZE)
+    }
+
+    fn zeroize(&mut self) {
+        let addr = self.index_to_addr();
+        unsafe {
+            core::ptr::write_bytes(addr as *mut u8, 0x0, GRANULE_SIZE);
+        }
+    }
+}
+
+pub struct Entry(Spinlock<Granule>);
+impl Entry {
+    pub fn new() -> Self {
+        Self(Spinlock::new(Granule {
+            state: GranuleState::Undelegated,
+        }))
+    }
+
+    pub fn lock(&self) -> Result<SpinlockGuard<'_, Granule>, Error> {
+        let granule = self.0.lock();
+        Ok(granule)
+    }
+}
--- rmm/src/granule/mod.rs
+++ rmm/src/granule/mod.rs
@@ -1,2 +1,9 @@
+#[cfg(feature = "gst_page_table")]
 pub mod page_table;
+#[cfg(feature = "gst_page_table")]
 pub use page_table::*;
+
+#[cfg(not(feature = "gst_page_table"))]
+pub mod array;
+#[cfg(not(feature = "gst_page_table"))]
+pub use array::*;

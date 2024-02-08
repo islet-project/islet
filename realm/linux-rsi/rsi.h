0d4a879c053ecd91b40e2030079438e113293f19
Lukasz Pawelczyk <l.pawelczyk@samsung.com>
2024-02-08 15:01:03 +0100

    Update RSI kernel module for EAC5 standard
    It uses new RSI API for VERSION, ATTESTATION_INIT and ATTESTATION_CONTINUE
    calls. Due to the changes in those calls it also updates the ioctl API it
    provides as the output token can be longer than 4k now. The ioctl API handles
    such cases. It's usage is described briefly in rsi.h header.
    
    Rust wrapper for the ioctl API is updated in the remote-attestation repository
    on eac5 branc.

--- realm/linux-rsi/rsi.h
+++ realm/linux-rsi/rsi.h
@@ -8,10 +8,32 @@
 #ifndef RSI_ABI_VERSION_GET_MINOR
 #define RSI_ABI_VERSION_GET_MINOR(_version) ((_version) & 0xFFFF)
 #endif
+#define RSI_ABI_VERSION_SET(_major, _minor) (((_major) << 16) | (_minor))
+
 
 #define MAX_MEASUR_LEN  0x40
 #define CHALLENGE_LEN   0x40
-#define MAX_TOKEN_LEN   0x1000
+#define GRANULE_LEN     0x1000
+
+/*
+ * Below is the definition of an ioctl API.
+ *
+ * ABI_VERSION: one output parameter: the lower version.
+ *
+ * MEASUREMENT_READ: index is an input parameter (0: RIM, 1-4: REMS), data_len
+ * and data are output parameters where the measurement is returned. Values
+ * smaller than 64 bytes (e.g. SHA256 -> 32 bytes) are padded with zeroes.
+ *
+ * MEASUREMENT_EXTEND: all paramaters are input, index (1-4: REMS), data and
+ * data_len are data to extend with (1-64 bytes).
+ *
+ * ATTESTATION_TOKEN: challange is an input parameter (exactly 64 bytes). Token
+ * is an allocated memory of token_len where the token will be returned. If the
+ * size is too small the kernel will return ERANGE and put the required amount
+ * in token_len. Calling it with token_len == 0 and token == NULL to get the
+ * required amount is allowed. Token is written in parts. If some error occurs
+ * during the call the content of token is undefined.
+ */
 
 struct rsi_measurement
 {
@@ -23,8 +45,8 @@ struct rsi_measurement
 struct rsi_attestation
 {
 	uint8_t challenge[CHALLENGE_LEN];
-	uint32_t token_len;
-	uint8_t token[MAX_TOKEN_LEN];
+	uint64_t token_len;
+	uint8_t *token;
 };
 
 #define RSIIO_ABI_VERSION          _IOR('x', 190, uint32_t /*version*/)
@@ -39,5 +61,5 @@ struct rsi_attestation
  *
  * This will not be required when the module is builtin in the kernel.
  */
-extern struct realm_config __attribute((aligned(MAX_TOKEN_LEN))) config;
-extern char __attribute__((aligned(MAX_TOKEN_LEN))) rsi_page_buf[MAX_TOKEN_LEN];
+extern struct realm_config __attribute((aligned(GRANULE_LEN))) config;
+extern char __attribute__((aligned(GRANULE_LEN))) rsi_page_buf[GRANULE_LEN];

a985865e0b34d6976a456260231ec2fa63926c93
Changho Choi <ch754.choi@samsung.com>
2024-03-07 14:20:29 +0900

    Fix build errors of creating a reference to mutable static
    This commit fixes the build errors of the latest rustc (2024-03-01).
    ```
    error: creating a shared reference to mutable static is discouraged
      --> plat/fvp/src/entry.rs:79:9
       |
    79 |     if (&COLD_BOOT as *const bool).read_volatile() {
       |         ^^^^^^^^^^ shared reference to mutable static
       |
       = note: for more information, see issue #114447 <https://github.com/rust-lang/rust/issues/114447>
       = note: this will be a hard error in the 2024 edition
       = note: this shared reference has lifetime `'static`, but if the static ever gets mutated, or a mutable reference is created, then any further use of this shared reference is Undefined Behavior
    note: the lint level is defined here
    help: use `addr_of!` instead to create a raw pointer
    
    error: creating a mutable reference to mutable static is discouraged
      --> plat/fvp/src/entry.rs:85:10
       |
    85 |         (&mut COLD_BOOT as *mut bool).write_volatile(false);
       |          ^^^^^^^^^^^^^^ mutable reference to mutable static
       |
       = note: for more information, see issue #114447 <https://github.com/rust-lang/rust/issues/114447>
       = note: this will be a hard error in the 2024 edition
       = note: this mutable reference has lifetime `'static`, but if the static gets accessed (read or written) by any other means, or any other reference is created, then any further use of this mutable reference is Undefined Behavior
    help: use `addr_of_mut!` instead to create a raw pointer
    ```
    
    Signed-off-by: Changho Choi <ch754.choi@samsung.com>

--- plat/fvp/src/entry.rs
+++ plat/fvp/src/entry.rs
@@ -2,6 +2,7 @@ use crate::allocator;
 use crate::log::LevelFilter;
 
 use armv9a::regs::*;
+use core::ptr::{addr_of, addr_of_mut};
 use islet_rmm::config::{NUM_OF_CPU, RMM_STACK_SIZE};
 use islet_rmm::io::stdout;
 use islet_rmm::logger;
@@ -76,12 +77,12 @@ unsafe fn init_mm() {
 unsafe fn setup() {
     static mut COLD_BOOT: bool = true;
 
-    if (&COLD_BOOT as *const bool).read_volatile() {
+    if (addr_of!(COLD_BOOT) as *const bool).read_volatile() {
         clear_bss();
         allocator::init();
         init_console();
         init_mm();
 
-        (&mut COLD_BOOT as *mut bool).write_volatile(false);
+        (addr_of_mut!(COLD_BOOT) as *mut bool).write_volatile(false);
     }
 }

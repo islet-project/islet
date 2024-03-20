d1431f94439aae326f2b88f3ae60c6ae17ee5f51
Changho Choi <ch754.choi@samsung.com>
2024-03-20 19:46:10 +0900

    ci: Fix build errors
    Signed-off-by: Changho Choi <ch754.choi@samsung.com>

--- scripts/init_min_acs.sh
+++ scripts/init_min_acs.sh
@@ -8,10 +8,12 @@ HERE=$ROOT/scripts
 # Install packages
 $HERE/deps/pkgs-for-ci.sh
 
-# Sync submodules (only assets, tf-a, acs)
+# Sync submodules (only assets, tf-a, acs, ciborium, and coset)
 git submodule update --init --depth 1 $ROOT/assets
 git submodule update --init --depth 1 $ROOT/third-party/tf-a
 git submodule update --init --depth 1 $ROOT/third-party/cca-rmm-acs
+git submodule update --init --depth 1 $ROOT/third-party/ciborium
+git submodule update --init --depth 1 $ROOT/third-party/coset
 
 # Install rust (min)
 $HERE/deps/rust_min.sh

411e3e79d572f7d0cf4030fd5b7d726b8c5bf6ab
Changho Choi <ch754.choi@samsung.com>
2024-03-20 20:23:02 +0900

    ci: Initialize ciborium and coset together with assets
    This commit will help to fix all the build errors in CI.
    
    Signed-off-by: Changho Choi <ch754.choi@samsung.com>

--- scripts/deps/assets.sh
+++ scripts/deps/assets.sh
@@ -5,3 +5,5 @@ set -e
 ROOT=$(git rev-parse --show-toplevel)
 
 cd $ROOT && git submodule update --init assets
+git submodule update --init --depth 1 $ROOT/third-party/ciborium
+git submodule update --init --depth 1 $ROOT/third-party/coset

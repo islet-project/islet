7a1185a92b6f77a923d711ba490f70799b1b7553
Changho Choi <ch754.choi@samsung.com>
2024-04-05 14:40:55 +0900

    Bump fvp to the latest release (11.25)
    The latest simulator contains additional useful plugins.
    
    [before]
    ArchMsgTrace.so
    libete-plugin.so
    ScalableVectorExtension.so
    TarmacTrace.so
    
    [after]
    ArchMsgTrace.so
    libete-plugin.so
    ScalableVectorExtension.so
    TarmacTrace.so
    (+) GenericTrace.so
    (+) ListTraceSources.so
    (+) GenericCounter.so
    
    Signed-off-by: Changho Choi <ch754.choi@samsung.com>

--- scripts/deps/simulator.sh
+++ scripts/deps/simulator.sh
@@ -4,8 +4,8 @@ set -e
 
 ROOT=$(git rev-parse --show-toplevel)
 FVP=$ROOT/third-party/fvp
-URL=https://developer.arm.com/-/media/Files/downloads/ecosystem-models/FVP_Base_RevC-2xAEMvA_11.20_15_Linux64.tgz
-TAR=FVP_Base_RevC-2xAEMvA_11.20_15_Linux64.tgz
+URL=https://developer.arm.com/-/media/Files/downloads/ecosystem-models/FM_11_25/FVP_Base_RevC-2xAEMvA_11.25_15_Linux64.tgz
+TAR=FVP_Base_RevC-2xAEMvA_11.25_15_Linux64.tgz
 
 if [ ! -d "$FVP" ]; then
 	mkdir $FVP

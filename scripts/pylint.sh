5a8b5b1916e29cfadeaf44c5ae70b2a47f4ddace
Sangwan Kwon <sangwan.kwon@samsung.com>
2024-04-05 15:33:25 +0900

    scripts: Print the accurate count of unsafe keywords
    - Cargo-geiger cannot analyze macros, hence the number is approximate.
    - Ref: https://doc.rust-lang.org/reference/unsafe-keyword.html)
    
    ```sh
    $ ./scripts/unsafe-analyzer
    Unsafe Keywords:
     - Total           | 199
     - Functions       | 35
     - Blocks          | 161
     - Impls           | 3
    
    Unsafe Usage (approximate figures):
     - Total           | 163
     - Deref Operation | 23
     - Unclassified    | 137
     - Skipped         | 3
    
    Unsafe Internal (approximate figures):
     - Exprs           | 407
     - Stmts           | 234
    ```
    
    Signed-off-by: Sangwan Kwon <sangwan.kwon@samsung.com>

--- scripts/pylint.sh
+++ scripts/pylint.sh
@@ -6,4 +6,4 @@ ROOT=$(git rev-parse --show-toplevel)
 HERE=$ROOT/scripts
 
 # TODO: Add fvp-cca
-pylint --disable=C0103,C0114,C0116,W0621,W1401,W1514 $HERE/unsafe-analyzer
+pylint --disable=C0103,C0114,C0115,C0116,W0621,W1401,W1514 $HERE/unsafe-analyzer

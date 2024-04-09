1df6def68e5410bd1cf85a8e771c38d45e0f2bd8
Changho Choi <ch754.choi@samsung.com>
2024-04-09 14:54:49 +0900

    refactor: Get rmi constraints without insertion
    This commit refactors RMI's constraint retrieval procedure. The main
    motivation of the refactoring is that insertion & get in the map are
    very expensive operations especially in the formal semantic and avoiding
    them is better if possible. It would be also beneficial for runtime
    performance, as the previous insertions are no longer involved. The
    assumption is that the configuration regarding constraints does not
    change at runtime.
    
    Signed-off-by: Changho Choi <ch754.choi@samsung.com>

--- rmm/src/rmi/constraint.rs
+++ rmm/src/rmi/constraint.rs
@@ -1,8 +1,5 @@
-extern crate alloc;
-
 use crate::event::Command;
 use crate::rmi;
-use alloc::collections::btree_map::BTreeMap;
 
 #[allow(dead_code)]
 #[derive(Default, Copy, Clone)]
@@ -23,73 +20,37 @@ impl Constraint {
     }
 }
 
-lazy_static! {
-    static ref CONSTRAINTS: BTreeMap<Command, Constraint> = {
-        let mut m = BTreeMap::new();
-        m.insert(rmi::VERSION, Constraint::new(rmi::VERSION, 1, 1));
-        m.insert(
-            rmi::GRANULE_DELEGATE,
-            Constraint::new(rmi::GRANULE_DELEGATE, 2, 1),
-        );
-        m.insert(
-            rmi::GRANULE_UNDELEGATE,
-            Constraint::new(rmi::GRANULE_UNDELEGATE, 2, 1),
-        );
-        m.insert(rmi::DATA_CREATE, Constraint::new(rmi::DATA_CREATE, 6, 1));
-        m.insert(rmi::DATA_CREATE_UNKNOWN, Constraint::new(rmi::DATA_CREATE_UNKNOWN, 4, 1));
-        m.insert(rmi::DATA_DESTROY, Constraint::new(rmi::DATA_DESTROY, 3, 3));
-        m.insert(
-            rmi::REALM_ACTIVATE,
-            Constraint::new(rmi::REALM_ACTIVATE, 2, 1),
-        );
+fn pick(cmd: Command) -> Option<Constraint> {
+    let constraint = match cmd {
+        rmi::VERSION => Constraint::new(rmi::VERSION, 1, 1),
+        rmi::GRANULE_DELEGATE => Constraint::new(rmi::GRANULE_DELEGATE, 2, 1),
+        rmi::GRANULE_UNDELEGATE => Constraint::new(rmi::GRANULE_UNDELEGATE, 2, 1),
+        rmi::DATA_CREATE => Constraint::new(rmi::DATA_CREATE, 6, 1),
+        rmi::DATA_CREATE_UNKNOWN => Constraint::new(rmi::DATA_CREATE_UNKNOWN, 4, 1),
+        rmi::DATA_DESTROY => Constraint::new(rmi::DATA_DESTROY, 3, 3),
+        rmi::REALM_ACTIVATE => Constraint::new(rmi::REALM_ACTIVATE, 2, 1),
         // NOTE: REALM_CREATE has 3 of arg_num and 1 of ret_num according to the specification,
         //       but we're using one more return value for our own purpose.
-        m.insert(rmi::REALM_CREATE, Constraint::new(rmi::REALM_CREATE, 3, 2));
-        m.insert(
-            rmi::REALM_DESTROY,
-            Constraint::new(rmi::REALM_DESTROY, 2, 1),
-        );
+        rmi::REALM_CREATE => Constraint::new(rmi::REALM_CREATE, 3, 2),
+        rmi::REALM_DESTROY => Constraint::new(rmi::REALM_DESTROY, 2, 1),
         // NOTE: REC_CREATE has 4 of arg_num and 1 of ret_num according to the specification,
         //       but we're using one more return value for our own purpose.
-        m.insert(rmi::REC_CREATE, Constraint::new(rmi::REC_CREATE, 4, 2));
-        m.insert(rmi::REC_DESTROY, Constraint::new(rmi::REC_DESTROY, 4, 1));
-        m.insert(rmi::REC_ENTER, Constraint::new(rmi::REC_ENTER, 3, 1));
-        m.insert(
-            rmi::RTT_MAP_UNPROTECTED,
-            Constraint::new(rmi::RTT_MAP_UNPROTECTED, 5, 1),
-        );
-        m.insert(
-            rmi::RTT_UNMAP_UNPROTECTED,
-            Constraint::new(rmi::RTT_UNMAP_UNPROTECTED, 4, 1),
-        );
-        m.insert(
-            rmi::RTT_READ_ENTRY,
-            Constraint::new(rmi::RTT_READ_ENTRY, 4, 5),
-        );
-        m.insert(rmi::FEATURES, Constraint::new(rmi::FEATURES, 2, 2));
-        m.insert(
-            rmi::REC_AUX_COUNT,
-            Constraint::new(rmi::REC_AUX_COUNT, 2, 2),
-        );
-        m.insert(
-            rmi::RTT_CREATE,
-            Constraint::new(rmi::RTT_CREATE, 5, 1),
-        );
-        m.insert(
-            rmi::RTT_DESTROY,
-            Constraint::new(rmi::RTT_DESTROY, 5, 1),
-        );
-        m.insert(
-            rmi::RTT_INIT_RIPAS,
-            Constraint::new(rmi::RTT_INIT_RIPAS, 4, 2),
-        );
-        m.insert(
-            rmi::RTT_SET_RIPAS,
-            Constraint::new(rmi::RTT_SET_RIPAS, 6, 2),
-        );
-        m.insert(rmi::REQ_COMPLETE, Constraint::new(rmi::REQ_COMPLETE, 4, 2));
-        m
+        rmi::REC_CREATE => Constraint::new(rmi::REC_CREATE, 4, 2),
+        rmi::REC_DESTROY => Constraint::new(rmi::REC_DESTROY, 4, 1),
+        rmi::REC_ENTER => Constraint::new(rmi::REC_ENTER, 3, 1),
+        rmi::RTT_MAP_UNPROTECTED => Constraint::new(rmi::RTT_MAP_UNPROTECTED, 5, 1),
+        rmi::RTT_UNMAP_UNPROTECTED => Constraint::new(rmi::RTT_UNMAP_UNPROTECTED, 4, 1),
+        rmi::RTT_READ_ENTRY => Constraint::new(rmi::RTT_READ_ENTRY, 4, 5),
+        rmi::FEATURES => Constraint::new(rmi::FEATURES, 2, 2),
+        rmi::REC_AUX_COUNT => Constraint::new(rmi::REC_AUX_COUNT, 2, 2),
+        rmi::RTT_CREATE => Constraint::new(rmi::RTT_CREATE, 5, 1),
+        rmi::RTT_DESTROY => Constraint::new(rmi::RTT_DESTROY, 5, 1),
+        rmi::RTT_INIT_RIPAS => Constraint::new(rmi::RTT_INIT_RIPAS, 4, 2),
+        rmi::RTT_SET_RIPAS => Constraint::new(rmi::RTT_SET_RIPAS, 6, 2),
+        rmi::REQ_COMPLETE => Constraint::new(rmi::REQ_COMPLETE, 4, 2),
+        _ => return None,
     };
+    Some(constraint)
 }
 
 pub fn validate<T, G>(cmd: Command, ok_func: T, else_func: G)
@@ -97,7 +58,7 @@ where
     T: Fn(usize, usize),
     G: FnOnce(), // TODO: error command?
 {
-    if let Some(c) = CONSTRAINTS.get(&cmd) {
+    if let Some(c) = pick(cmd) {
         // TODO: command-specific validation routine if needed
         ok_func(c.arg_num, c.ret_num);
     } else {

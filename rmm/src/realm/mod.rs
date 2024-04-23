820e3ec8fb874f09481213a9afc60b55c22194fe
Changho Choi <ch754.choi@samsung.com>
2024-04-23 17:24:31 +0900

    refactor: Finish integrating realm into rd
    Realm and Rd (Realm Descriptor) have almost the same purpose.
    The commit aims to keep only Rd which is defined by spec.
    
    Related to #230
    
    [Summary of changes]
    
    - Remove `struct Realm`, `RMS`, and `get_realm()`
    - Use VMID_SET instead of the previous RMS
    - Move vmid from Realm to Rd
    - Integrate vmid with id (use only vmid passed by the Host)
    
    [A caution]
    
    - Data aborts are generated during realm creation for some reasons
    
    Signed-off-by: Changho Choi <ch754.choi@samsung.com>

--- rmm/src/realm/mod.rs
+++ rmm/src/realm/mod.rs
@@ -4,27 +4,3 @@ pub mod mm;
 pub mod registry;
 pub mod timer;
 pub mod vcpu;
-
-use alloc::sync::Arc;
-use core::fmt::Debug;
-use spin::mutex::Mutex;
-
-extern crate alloc;
-
-#[derive(Debug)]
-pub struct Realm {
-    id: usize,
-    pub vmid: u16,
-}
-
-impl Realm {
-    pub fn new(id: usize, vmid: u16) -> Arc<Mutex<Self>> {
-        Arc::new(Mutex::new(Self { id, vmid }))
-    }
-}
-
-impl Drop for Realm {
-    fn drop(&mut self) {
-        info!("Realm #{} was destroyed!", self.id);
-    }
-}
--- rmm/src/realm/registry.rs
+++ rmm/src/realm/registry.rs
@@ -1,14 +1,4 @@
-use crate::realm::Realm;
-
-use alloc::collections::BTreeMap;
-use alloc::sync::Arc;
-use spin::mutex::Mutex;
+use alloc::collections::BTreeSet;
 use spinning_top::Spinlock;
 
-type RealmMutex = Arc<Mutex<Realm>>;
-type RealmMap = BTreeMap<usize, RealmMutex>;
-pub static RMS: Spinlock<(usize, RealmMap)> = Spinlock::new((0, BTreeMap::new()));
-
-pub fn get_realm(id: usize) -> Option<RealmMutex> {
-    RMS.lock().1.get(&id).map(Arc::clone)
-}
+pub static VMID_SET: Spinlock<BTreeSet<usize>> = Spinlock::new(BTreeSet::new());

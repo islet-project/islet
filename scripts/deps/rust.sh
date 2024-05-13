06c859f4125a3327ddc1f24f06b75e34edc5dc29
Changho Choi <ch754.choi@samsung.com>
2024-05-13 16:47:38 +0900

    Revert "scripts: Remove cargo install to avoid a feature error"
    This reverts commit daa1cf9f2d170b28b67cc9028f0448dfd3a98ad9 (#322),
    as the previous issue in AnalysisHub has been alternatively resolved
    by rust toolchain update (#323).
    
    Signed-off-by: Changho Choi <ch754.choi@samsung.com>

--- scripts/deps/rust.sh
+++ scripts/deps/rust.sh
@@ -18,4 +18,6 @@ rustup component add rust-src rustfmt
 rustup target add aarch64-unknown-linux-gnu
 rustup component add clippy
 
+cargo install cargo-bloat cbindgen mdbook
+
 rustc --version --verbose

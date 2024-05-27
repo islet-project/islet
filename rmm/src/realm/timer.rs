bd98385b02417aa62d5c19e3a4a5320681a03ba0
Sangwan Kwon <sangwan.kwon@samsung.com>
2024-05-27 17:55:30 +0900

    rmm: Fix to use aarch64-cpu for Counter-Timer regs
    When the Effective value of HCR_EL2.E2H is 0, bit 11 is RES0.
    So, EL1PTEN has been removed.
    
    ref) "https://developer.arm.com/documentation/ddi0601/"
         "2023-09/AArch64-Registers/"
         "CNTHCTL-EL2--Counter-timer-Hypervisor-Control-Register?lang=en"
    
    Signed-off-by: Sangwan Kwon <sangwan.kwon@samsung.com>

--- rmm/src/realm/timer.rs
+++ rmm/src/realm/timer.rs
@@ -2,11 +2,11 @@ use crate::rec::Rec;
 use crate::rmi::error::Error;
 use crate::rmi::rec::run::Run;
 
-use armv9a::regs::*;
+use aarch64_cpu::registers::*;
 
 pub fn init_timer(rec: &mut Rec<'_>) {
     let timer = &mut rec.context.timer;
-    timer.cnthctl_el2 = CNTHCTL_EL2::EL1PTEN | CNTHCTL_EL2::EL1PCEN | CNTHCTL_EL2::EL1PCTEN;
+    timer.cnthctl_el2 = (CNTHCTL_EL2::EL1PCEN::SET + CNTHCTL_EL2::EL1PCTEN::SET).into();
 }
 
 pub fn set_cnthctl(rec: &mut Rec<'_>, val: u64) {
@@ -17,25 +17,25 @@ pub fn set_cnthctl(rec: &mut Rec<'_>, val: u64) {
 pub fn restore_state(rec: &Rec<'_>) {
     let timer = &rec.context.timer;
 
-    unsafe { CNTVOFF_EL2.set(timer.cntvoff_el2) };
-    unsafe { CNTPOFF_EL2.set(timer.cntpoff_el2) };
-    unsafe { CNTV_CVAL_EL0.set(timer.cntv_cval_el0) };
-    unsafe { CNTV_CTL_EL0.set(timer.cntv_ctl_el0) };
-    unsafe { CNTP_CVAL_EL0.set(timer.cntp_cval_el0) };
-    unsafe { CNTP_CTL_EL0.set(timer.cntp_ctl_el0) };
-    unsafe { CNTHCTL_EL2.set(timer.cnthctl_el2) };
+    CNTVOFF_EL2.set(timer.cntvoff_el2);
+    CNTPOFF_EL2.set(timer.cntpoff_el2);
+    CNTV_CVAL_EL0.set(timer.cntv_cval_el0);
+    CNTV_CTL_EL0.set(timer.cntv_ctl_el0);
+    CNTP_CVAL_EL0.set(timer.cntp_cval_el0);
+    CNTP_CTL_EL0.set(timer.cntp_ctl_el0);
+    CNTHCTL_EL2.set(timer.cnthctl_el2);
 }
 
 pub fn save_state(rec: &mut Rec<'_>) {
     let timer = &mut rec.context.timer;
 
-    timer.cntvoff_el2 = unsafe { CNTVOFF_EL2.get() };
-    timer.cntv_cval_el0 = unsafe { CNTV_CVAL_EL0.get() };
-    timer.cntv_ctl_el0 = unsafe { CNTV_CTL_EL0.get() };
-    timer.cntpoff_el2 = unsafe { CNTPOFF_EL2.get() };
-    timer.cntp_cval_el0 = unsafe { CNTP_CVAL_EL0.get() };
-    timer.cntp_ctl_el0 = unsafe { CNTP_CTL_EL0.get() };
-    timer.cnthctl_el2 = unsafe { CNTHCTL_EL2.get() };
+    timer.cntvoff_el2 = CNTVOFF_EL2.get();
+    timer.cntv_cval_el0 = CNTV_CVAL_EL0.get();
+    timer.cntv_ctl_el0 = CNTV_CTL_EL0.get();
+    timer.cntpoff_el2 = CNTPOFF_EL2.get();
+    timer.cntp_cval_el0 = CNTP_CVAL_EL0.get();
+    timer.cntp_ctl_el0 = CNTP_CTL_EL0.get();
+    timer.cnthctl_el2 = CNTHCTL_EL2.get();
 }
 
 pub fn send_state_to_host(rec: &Rec<'_>, run: &mut Run) -> Result<(), Error> {

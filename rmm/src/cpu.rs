4d8f3a73041a09bd66b5d9131191a2cb38c03ebd
Sangwan Kwon <sangwan.kwon@samsung.com>
2024-05-30 10:42:22 +0900

    rmm: Fix to use aarch64_cpu::register::MPIDR_EL1
    Signed-off-by: Sangwan Kwon <sangwan.kwon@samsung.com>

--- rmm/src/cpu.rs
+++ rmm/src/cpu.rs
@@ -1,6 +1,6 @@
 use crate::config::NUM_OF_CPU_PER_CLUSTER;
 
-use armv9a::regs::*;
+use aarch64_cpu::registers::*;
 
 #[no_mangle]
 pub extern "C" fn get_cpu_id() -> usize {
@@ -10,10 +10,8 @@ pub extern "C" fn get_cpu_id() -> usize {
 
 #[inline(always)]
 pub fn id() -> (usize, usize) {
-    unsafe {
-        (
-            MPIDR_EL1.get_masked_value(MPIDR_EL1::AFF2) as usize,
-            MPIDR_EL1.get_masked_value(MPIDR_EL1::AFF1) as usize,
-        )
-    }
+    (
+        MPIDR_EL1.read(MPIDR_EL1::Aff2) as usize,
+        MPIDR_EL1.read(MPIDR_EL1::Aff1) as usize,
+    )
 }

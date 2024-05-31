d30a89573b3d8f294f46b3cae52e59532127a40a
Sangwan Kwon <sangwan.kwon@samsung.com>
2024-05-31 14:36:26 +0900

    armv9a: Deprecate most system registers
    Signed-off-by: Sangwan Kwon <sangwan.kwon@samsung.com>

--- lib/armv9a/src/regs.rs
+++ lib/armv9a/src/regs.rs
@@ -1,29 +1,5 @@
 use crate::bits_in_reg;
 
-define_sys_register!(
-    MPIDR_EL1, // ref. D7.2.74
-    RES1[31 - 31],
-    AFF2[23 - 16], // Affinity level 2
-    AFF1[15 - 8]   // Affinity level 1
-);
-
-define_sys_register!(CurrentEL, EL[3 - 2]);
-pub fn current_el() -> u8 {
-    unsafe { CurrentEL.get_masked_value(CurrentEL::EL) as u8 }
-}
-
-define_sys_register!(VBAR_EL2, RES0[10 - 0]);
-
-define_sys_register!(
-    ESR_EL1,
-    // Exception Class.
-    EC[31 - 26],
-    // Instruction Length for synchronous exceptions.
-    IL[25 - 25],
-    // Syndrome information.
-    ISS[24 - 0]
-);
-
 define_bits!(
     EsrEl1,
     // Exception Class.
@@ -111,162 +87,6 @@ pub const NON_EMULATABLE_ABORT_MASK: u64 =
 pub const EMULATABLE_ABORT_MASK: u64 =
     NON_EMULATABLE_ABORT_MASK | EsrEl2::ISV | EsrEl2::SAS | EsrEl2::SF | EsrEl2::WNR;
 
-define_register!(SP);
-define_sys_register!(SP_EL0);
-define_sys_register!(SP_EL1);
-define_sys_register!(SP_EL2);
-
-define_sys_register!(
-    SPSR_EL2,
-    D[9 - 9], // Debug exception mask
-    A[8 - 8], // SError exception mask
-    I[7 - 7], // IRQ exception mask
-    F[6 - 6], // FIQ exception mask
-    M[3 - 0]  // Exception level and selected SP
-);
-
-define_sys_register!(SPSR_EL1);
-define_sys_register!(ELR_EL1);
-define_sys_register!(ELR_EL2);
-define_sys_register!(TPIDR_EL2);
-
-define_sys_register!(
-    HCR_EL2, // ref. D13.2.46\
-    FWB[46 - 46],
-    TEA[37 - 37],
-    TERR[36 - 36],
-    TLOR[35 - 35],
-    E2H[34 - 34],
-    ID[33 - 33],    // Disables stage 2 instruction cache
-    CD[32 - 32],    // Disables stage 2 data cache
-    RW[31 - 31],    // Execution state control for lower Exception level
-    TRVM[30 - 30],  // Trap reads of Virtual Memory controls
-    TDZ[28 - 28],   // Traps DC ZVA instruction
-    TGE[27 - 27],   // Traps general exceptions
-    TVM[26 - 26],   // Traps virtual memory controls
-    TTLB[25 - 25],  // Traps TLB maintenance instructions
-    TPU[24 - 24],   // Traps cache maintenance instructions to Point of Unification (POU)
-    TPC[23 - 23], // Traps data or unified cache maintenance instructions to Point of Coherency (POC)
-    TSW[22 - 22], // Traps data or unified cache maintenance instructions by Set or Way
-    TACR[21 - 21], // Traps Auxiliary Control registers
-    TIDCP[20 - 20], // Traps Implementation Dependent functionality
-    TSC[19 - 19], // Traps SMC instruction.
-    TID3[18 - 18], // Traps ID group 3
-    TID2[17 - 17], // Traps ID group 2
-    TID1[16 - 16], // Traps ID group 1
-    TID0[15 - 15], // Traps ID group 0
-    TWE[14 - 14], // Traps WFE instruction
-    TWI[13 - 13], // Traps WFI instruction
-    DC[12 - 12],  // Default cacheable
-    BSU[11 - 10], // Barrier shareability upgrade
-    FB[9 - 9],    // Forces broadcast
-    VSE[8 - 8],   // Virtual System Error/Asynchronous Abort.
-    VI[7 - 7],    // Virtual IRQ interrupt
-    VF[6 - 6],    // Virtual FRQ interrupt
-    AMO[5 - 5],   // Asynchronous abort and error interrupt routing
-    IMO[4 - 4],   // Physical IRQ routing
-    FMO[3 - 3],   // Physical FIQ routing
-    PTW[2 - 2],   // Protected Table Walk
-    SWIO[1 - 1],
-    VM[0 - 0],               // Virtualization enable
-    RES0[63 - 34 | 29 - 29]  //RES1[1 - 1]
-);
-
-define_sys_register!(
-    SCTLR_EL2,
-    EE[25 - 25],  // Endianness of data accesses at EL2
-    WXN[19 - 19], // Write permission implies Execute-never
-    I[12 - 12],   // Instruction access Cacheability at EL2
-    EOS[11 - 11], // Exception exit is a context synchronization event
-    SA[3 - 3],    // SP Alignment check enable
-    C[2 - 2],     // Data access Cacheability  at EL2
-    A[1 - 1],     // Alignment check enable
-    M[0 - 0]      // MMU enable for EL2
-);
-
-define_sys_register!(
-    ID_AA64MMFR0_EL1, // ref. D7.2.43: AArch64 Memory Model Feature Register 0
-    TGran4[31 - 28],
-    TGran64[27 - 24],
-    TGran16[23 - 20],
-    BigEndEL0[19 - 16],
-    SNSMem[15 - 12],
-    BigEnd[11 - 8],
-    ASIDBits[7 - 4],
-    PARange[3 - 0]
-);
-
-define_sys_register!(
-    ID_AA64MMFR1_EL1, // ref. D19.2.65: AArch64 Memory Model Feature Register 1
-    CMOW[59 - 56],
-    TIDCP1[55 - 52],
-    nTLBPA[51 - 48],
-    AFP[47 - 44],
-    HCX[43 - 40],
-    ETS[39 - 36],
-    TWED[35 - 32],
-    XNX[31 - 28],
-    SpecSEI[27 - 24],
-    PAN[23 - 20],
-    LO[19 - 16],
-    HPDS[15 - 12],
-    VH[11 - 8],
-    VMID[7 - 4],
-    HAFDBS[3 - 0]
-);
-
-pub mod mmfr1_vmid {
-    pub const VMIDBITS_8: u64 = 0;
-    pub const VMIDBITS_16: u64 = 2;
-}
-
-define_sys_register!(
-    MAIR_EL2, // ref. D7.2.71: Memory Attribute Indirection Register
-    Attr7[63 - 56],
-    Attr6[55 - 48],
-    Attr5[47 - 40],
-    Attr4[39 - 32],
-    Attr3[31 - 24],
-    Attr2[23 - 16],
-    Attr1[15 - 8],
-    Attr0[7 - 0]
-);
-
-pub mod mair_attr {
-    // N: non
-    // G: Gathering, R: Reodering, E: Early write-back
-    pub const DEVICE_NGNRNE: u64 = 0b0000_0000; // 0x0
-    pub const DEVICE_NGNRE: u64 = 0b0000_0100; // 0x4
-    pub const DEVICE_GRE: u64 = 0b0000_1100; // 0xc
-    pub const NORMAL_NC: u64 = 0b0100_0100; // 0x44, normal memory, non-cacheable
-    pub const NORMAL: u64 = 0b1111_1111; // 0xff, nomral memory, inner read-alloc, write-alloc,wb, non-transient
-}
-
-define_sys_register!(
-    TTBR0_EL2, // ref. Translation Table Base Register 0(EL2)
-    ASID[63 - 48],
-    BADDR[47 - 1],
-    CNP[0 - 0]
-);
-
-define_sys_register!(
-    TCR_EL2, // ref. Translation Control Register (EL2)
-    MTX[33 - 33],
-    DS[32 - 32],
-    TCMA[30 - 30],
-    TBID[29 - 29],
-    HPD[24 - 24],
-    HD[22 - 22],
-    HA[21 - 21],
-    TBI[20 - 20],
-    PS[18 - 16],
-    TG0[15 - 14],
-    SH0[13 - 12],
-    ORGN0[11 - 10],
-    IRGN0[9 - 8],
-    T0SZ[5 - 0]
-);
-
 define_sys_register!(
     VTCR_EL2, // ref. Virtualzation Translation Control Register
     DS[32 - 32],
@@ -345,116 +165,6 @@ pub mod tcr_start_level {
     pub const L0: u64 = 0b10;
 }
 
-define_sys_register!(
-    VTTBR_EL2,
-    VMID[63 - 48], // The VMID for the translation table
-    BADDR[47 - 0]  // Translation table base address
-);
-
-define_sys_register!(
-    HPFAR_EL2, // Ref. D13.2.55
-    NS[63 - 63],
-    FIPA[43 - 4] //
-);
-
-define_bits!(
-    HpfarEl2, // Ref. D13.2.55
-    NS[63 - 63],
-    FIPA[43 - 4] //
-);
-
-define_sys_register!(
-    FAR_EL2, // Ref. D13.2.55
-    OFFSET[11 - 0]
-);
-
-define_sys_register!(FAR_EL1);
-
-define_sys_register!(CPTR_EL2, TAM[30 - 30]);
-
-// GIC-related
-define_sys_register!(
-    ICH_VTR_EL2,  // Ref. Interrupt Controller VGIC Type Register
-    PRI[31 - 29], // The number of virtual priority bits implemented, minus one.
-    PRE[28 - 26], // The number of virtual preemption bits implemented, minus one.
-    ID[25 - 23], // The number of virtual interrupt identifier bits supported (0b000 means 16 bits while 0b001 means 24 bits)
-    LIST[4 - 0]  // The number of implemented List registers, minus one
-);
-
-define_sys_register!(ICH_LR0_EL2);
-define_sys_register!(ICH_LR1_EL2);
-define_sys_register!(ICH_LR2_EL2);
-define_sys_register!(ICH_LR3_EL2);
-define_sys_register!(ICH_LR4_EL2);
-define_sys_register!(ICH_LR5_EL2);
-define_sys_register!(ICH_LR6_EL2);
-define_sys_register!(ICH_LR7_EL2);
-define_sys_register!(ICH_LR8_EL2);
-define_sys_register!(ICH_LR9_EL2);
-define_sys_register!(ICH_LR10_EL2);
-define_sys_register!(ICH_LR11_EL2);
-define_sys_register!(ICH_LR12_EL2);
-define_sys_register!(ICH_LR13_EL2);
-define_sys_register!(ICH_LR14_EL2);
-define_sys_register!(ICH_LR15_EL2);
-
-define_sys_register!(ICH_AP0R0_EL2);
-define_sys_register!(ICH_AP0R1_EL2);
-define_sys_register!(ICH_AP0R2_EL2);
-define_sys_register!(ICH_AP0R3_EL2);
-define_sys_register!(ICH_AP1R0_EL2);
-define_sys_register!(ICH_AP1R1_EL2);
-define_sys_register!(ICH_AP1R2_EL2);
-define_sys_register!(ICH_AP1R3_EL2);
-
-define_sys_register!(ICH_VMCR_EL2);
-define_sys_register!(ICH_HCR_EL2);
-define_sys_register!(ICH_MISR_EL2);
-
-define_sys_register!(ICC_CTLR_EL1, EXT_RANGE[19 - 19]);
-
-define_sys_register!(
-    ICC_SRE_EL2,
-    ENABLE[3 - 3],
-    DIB[2 - 2],
-    DFB[1 - 1],
-    SRE[0 - 0]
-);
-
-define_bits!(
-    ICH_LR,
-    STATE[63 - 62],
-    HW[61 - 61],
-    GROUP[60 - 60],
-    NMI[59 - 59],
-    PRIORITY[55 - 48],
-    EOI[41 - 41],
-    PINTID[44 - 32],
-    VINTID[31 - 0]
-);
-
-pub mod ich_lr_state {
-    pub const INVALID: u64 = 0b00;
-    pub const PENDING: u64 = 0b01;
-    pub const ACTIVE: u64 = 0b10;
-    pub const PENDING_ACTIVE: u64 = 0b11;
-}
-
-define_sys_register!(
-    CNTHCTL_EL2,
-    EL1PTEN[11 - 11],
-    EL1PCEN[1 - 1],
-    EL1PCTEN[0 - 0]
-);
-define_sys_register!(CNTPOFF_EL2);
-define_sys_register!(CNTP_CTL_EL0);
-define_sys_register!(CNTP_CVAL_EL0);
-define_sys_register!(CNTVCT_EL0);
-define_sys_register!(CNTVOFF_EL2);
-define_sys_register!(CNTV_CTL_EL0);
-define_sys_register!(CNTV_CVAL_EL0);
-define_sys_register!(CNTV_TVAL_EL0);
-
 macro_rules! define_iss_id {
     ($name:ident, $Op0:expr, $Op1:expr, $CRn:expr, $CRm:expr, $Op2:expr) => {
         pub const $name: u32 = bits_in_reg(ISS::Op0, $Op0) as u32
@@ -477,60 +187,24 @@ define_bits!(
     Direction[0 - 0]
 );
 
-define_sys_register!(ID_AA64PFR0_EL1);
-define_bits!(AA64PFR0, AMU[47 - 44], SVE[35 - 32]);
 define_iss_id!(ISS_ID_AA64PFR0_EL1, 3, 0, 0, 4, 0);
 
-define_sys_register!(ID_AA64PFR1_EL1);
-define_bits!(AA64PFR1, MTE[11 - 8]);
 define_iss_id!(ISS_ID_AA64PFR1_EL1, 3, 0, 0, 4, 1);
 
-// TODO: current compiler doesn't understand this sysreg
-//define_sys_register!(ID_AA64ZFR0_EL1);
-//define_iss_id!(ISS_ID_AA64ZFR0_EL1, 3, 0, 0, 4, 4);
-
-define_sys_register!(ID_AA64DFR0_EL1);
-define_bits!(
-    AA64DFR0,
-    BRBE[55 - 52],
-    MTPMU[51 - 48],
-    TraceBuffer[47 - 44],
-    TraceFilt[43 - 40],
-    PMSVer[35 - 32],
-    CTX_CMPs[31 - 28],
-    WRPs[23 - 20],
-    BRPs[15 - 12],
-    PMUVer[11 - 8],
-    TraceVer[7 - 4],
-    DebugVer[3 - 0]
-);
 define_iss_id!(ISS_ID_AA64DFR0_EL1, 3, 0, 0, 5, 0);
 
-define_sys_register!(ID_AA64DFR1_EL1);
 define_iss_id!(ISS_ID_AA64DFR1_EL1, 3, 0, 0, 5, 1);
 
-define_sys_register!(ID_AA64AFR0_EL1);
 define_iss_id!(ISS_ID_AA64AFR0_EL1, 3, 0, 0, 5, 4);
 
-define_sys_register!(ID_AA64AFR1_EL1);
 define_iss_id!(ISS_ID_AA64AFR1_EL1, 3, 0, 0, 5, 5);
 
-define_sys_register!(ID_AA64ISAR0_EL1);
 define_iss_id!(ISS_ID_AA64ISAR0_EL1, 3, 0, 0, 6, 0);
 
-define_sys_register!(ID_AA64ISAR1_EL1);
-define_bits!(
-    AA64ISAR1,
-    GPI[31 - 28],
-    GPA[27 - 24],
-    APA[7 - 4],
-    API[11 - 8]
-);
 define_iss_id!(ISS_ID_AA64ISAR1_EL1, 3, 0, 0, 6, 1);
 
 define_iss_id!(ISS_ID_AA64MMFR0_EL1, 3, 0, 0, 7, 0);
 
 define_iss_id!(ISS_ID_AA64MMFR1_EL1, 3, 0, 0, 7, 1);
 
-define_sys_register!(ID_AA64MMFR2_EL1);
 define_iss_id!(ISS_ID_AA64MMFR2_EL1, 3, 0, 0, 7, 2);

9e82e3ecf48932eaa5a4841fd6fe8fd68076e6e6
Sangwan Kwon <sangwan.kwon@samsung.com>
2024-05-31 14:57:50 +0900

    armv9a: Deprecate VTCR_EL2 register
    Signed-off-by: Sangwan Kwon <sangwan.kwon@samsung.com>

--- rmm/src/rmi/rec/vtcr.rs
+++ rmm/src/rmi/rec/vtcr.rs
@@ -2,10 +2,7 @@ use crate::realm::rd::Rd;
 use crate::rec::Rec;
 use crate::rmi::error::Error;
 
-use aarch64_cpu::registers::Readable;
-use aarch64_cpu::registers::ID_AA64MMFR1_EL1;
-use armv9a::bits_in_reg;
-use armv9a::regs::*;
+use aarch64_cpu::registers::*;
 
 fn is_feat_vmid16_present() -> bool {
     ID_AA64MMFR1_EL1.read(ID_AA64MMFR1_EL1::VMIDBits) == ID_AA64MMFR1_EL1::VMIDBits::Bits16.into()
@@ -25,34 +22,32 @@ pub fn prepare_vtcr(rd: &Rd) -> Result<u64, Error> {
         return Err(Error::RmiErrorInput);
     }
 
-    let mut vtcr_val = bits_in_reg(VTCR_EL2::PS, tcr_paddr_size::PS_1T)
-        | bits_in_reg(VTCR_EL2::TG0, tcr_granule::G_4K)
-        | bits_in_reg(VTCR_EL2::SH0, tcr_shareable::INNER)
-        | bits_in_reg(VTCR_EL2::ORGN0, tcr_cacheable::WBWA)
-        | bits_in_reg(VTCR_EL2::IRGN0, tcr_cacheable::WBWA)
-        | bits_in_reg(VTCR_EL2::NSA, 1)
-        | bits_in_reg(VTCR_EL2::RES1, 1); //XXX: not sure why RES1 is in default set in tf-rmm
+    let mut vtcr_val = VTCR_EL2::PS::PA_40B_1TB
+        + VTCR_EL2::TG0::Granule4KB
+        + VTCR_EL2::SH0::Inner
+        + VTCR_EL2::ORGN0::NormalWBRAWA
+        + VTCR_EL2::IRGN0::NormalWBRAWA
+        + VTCR_EL2::NSA::NonSecurePASpace
+        + VTCR_EL2::RES1.val(1); //XXX: not sure why RES1 is in default set in tf-rmm
 
     if is_feat_vmid16_present() {
-        vtcr_val |= bits_in_reg(VTCR_EL2::VS, 1);
+        vtcr_val += VTCR_EL2::VS::Bits16;
     }
 
-    let sl0_array: [u64; 4] = [
-        vtcr_sl0::SL0_4K_L0,
-        vtcr_sl0::SL0_4K_L1,
-        vtcr_sl0::SL0_4K_L2,
-        vtcr_sl0::SL0_4K_L3,
+    let sl0_array = [
+        VTCR_EL2::SL0::Granule4KBLevel0,
+        VTCR_EL2::SL0::Granule4KBLevel1,
+        VTCR_EL2::SL0::Granule4KBLevel2,
+        VTCR_EL2::SL0::Granule4KBLevel3,
     ];
     let sl0_val = sl0_array[s2_starting_level as usize];
     let t0sz_val = (64 - ipa_bits) as u64;
 
-    vtcr_val |= bits_in_reg(VTCR_EL2::SL0, sl0_val) | bits_in_reg(VTCR_EL2::T0SZ, t0sz_val);
+    vtcr_val += sl0_val + VTCR_EL2::T0SZ.val(t0sz_val);
 
-    Ok(vtcr_val)
+    Ok(vtcr_val.into())
 }
 
 pub fn activate_stage2_mmu(rec: &Rec<'_>) {
-    unsafe {
-        VTCR_EL2.set(rec.vtcr());
-    }
+    VTCR_EL2.set(rec.vtcr());
 }

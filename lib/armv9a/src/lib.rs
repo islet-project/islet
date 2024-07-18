4eb14ff9f1a93927792c619e6a021b1c6841d403
Michał Szaknis <m.szaknis@samsung.com>
2024-07-18 14:09:21 +0200

    Revert "Use DUCT-TAPE to fix irq test"
    This reverts commit 2e0f9ffa672f526c91b5a6128cb70df082f33fd4.
    
    Signed-off-by: Michał Szaknis <m.szaknis@samsung.com>

--- lib/armv9a/src/lib.rs
+++ lib/armv9a/src/lib.rs
@@ -11,16 +11,3 @@ pub use regs::*;
 pub const fn bits_in_reg(mask: u64, val: u64) -> u64 {
     (val << (mask.trailing_zeros())) & mask
 }
-
-pub fn is_irq_pending() -> bool {
-    let val: u64;
-
-    unsafe {
-        core::arch::asm!(
-            "mrs {}, ISR_EL1",
-            out(reg) val
-        )
-    }
-
-    val != 0
-}

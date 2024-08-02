581846f492ff29cf6b7a2ac942cb4992a316cd6f
Zofia Abramowska <z.abramowska@samsung.com>
2024-08-02 12:07:55 +0200

    veraison: fixes and cleanup
    * bootstrap.sh - clean docker deployment, when "services" exists
    * change veraison services address to "https://" - this is currently
      by default
    * install `arc` package
    * remove ear.jwt from commited files
    
    Signed-off-by: Zofia Abramowska <z.abramowska@samsung.com>

--- examples/veraison/provisioning/run.sh
+++ examples/veraison/provisioning/run.sh
@@ -83,13 +83,13 @@ cocli corim create --template=corim.json --comid=endorsements.cbor --comid=refva
 
 loginfo "Provisioning generated Corim"
 cocli corim submit --corim-file=corim.cbor \
-    --api-server="http://provisioning-service:8888/endorsement-provisioning/v1/submit" \
+    --api-server="https://provisioning-service:8888/endorsement-provisioning/v1/submit" \
     '--media-type='\''application/corim-unsigned+cbor; profile=http://arm.com/cca/ssd/1'\'''
 
 ##### Verifying as relaying party
 
 loginfo "Verifying token as relaying party"
 evcli cca verify-as relying-party \
-    --api-server=http://verification-service:8080/challenge-response/v1/newSession \
+    --api-server=https://verification-service:8080/challenge-response/v1/newSession \
     --token=$TOKEN | tr -d '"' > ear.jwt
 arc verify -p=pkey.jwk ear.jwt

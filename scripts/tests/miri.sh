417606d8cf0a090150e8b079db1d24521d11b851
Sangwan Kwon <sangwan.kwon@samsung.com>
2024-08-30 09:25:14 +0900

    tool: Update crate & miri test scripts
    Signed-off-by: Sangwan Kwon <sangwan.kwon@samsung.com>

--- scripts/tests/miri.sh
+++ scripts/tests/miri.sh
@@ -6,7 +6,24 @@ mkdir -p $ROOT/out
 
 cd $ROOT/rmm
 
-MIRIFLAGS="-Zmiri-disable-stacked-borrows" cargo miri test --target aarch64-unknown-linux-gnu
+# rmi::features::test::rmi_features
+# rmi::gpt::test::rmi_granule_delegate_negative
+# rmi::gpt::test::rmi_granule_delegate_positive
+# rmi::gpt::test::rmi_granule_undelegate
+# rmi::realm::test::rmi_realm_create_negative
+# rmi::realm::test::rmi_realm_create_positive
+# rmi::version::test::rmi_version
+if [ $# -eq 0 ]; then
+	MIRIFLAGS="-Zmiri-disable-stacked-borrows" cargo miri test \
+		rmi::features::test::rmi_features \
+		--target aarch64-unknown-linux-gnu -- --nocapture
+	MIRIFLAGS="-Zmiri-disable-stacked-borrows" cargo miri test \
+		rmi::version::test::rmi_version \
+		--target aarch64-unknown-linux-gnu -- --nocapture
+else
+	MIRIFLAGS="-Zmiri-disable-stacked-borrows" cargo miri test $1 \
+		--target aarch64-unknown-linux-gnu -- --nocapture
+fi
 
 if [ $? -ne 0 ]; then
 	exit 1

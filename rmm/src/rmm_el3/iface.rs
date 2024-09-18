304a6750ee8644eeb1b102d432e158160f59fe9d
Lukasz Pawelczyk <l.pawelczyk@samsung.com>
2024-09-18 13:41:14 +0200

    Get VHUKS from TF-A using vendor SMC RMM calls
    Signed-off-by: Lukasz Pawelczyk <l.pawelczyk@samsung.com>

--- rmm/src/rmm_el3/iface.rs
+++ rmm/src/rmm_el3/iface.rs
@@ -1,11 +1,14 @@
 use super::{digest, utils};
 use super::{
     ATTEST_KEY_CURVE_ECC_SECP384R1, PLAT_TOKEN, REALM_ATTEST_KEY, RMM_SHARED_BUFFER_LOCK,
-    SHA256_DIGEST_SIZE,
+    SHA256_DIGEST_SIZE, VHUK_A, VHUK_M,
 };
 use crate::asm::smc;
 use crate::{config, rmi};
-use spinning_top::SpinlockGuard;
+use spinning_top::{Spinlock, SpinlockGuard};
+
+const ID_VHUK_A: usize = 0x1;
+const ID_VHUK_M: usize = 0x2;
 
 #[derive(Debug)]
 enum RmmEl3IfcError {
@@ -30,58 +33,94 @@ impl From<isize> for RmmEl3IfcError {
 }
 
 pub(super) fn get_realm_attest_key() {
-    trace!("GET_REALM_ATTEST_KEY");
+    trace!("RMM_GET_REALM_ATTEST_KEY");
 
     let guard: SpinlockGuard<'_, _> = super::RMM_SHARED_BUFFER_LOCK.lock();
 
     let ret = smc(
-        rmi::GET_REALM_ATTEST_KEY,
+        rmi::RMM_GET_REALM_ATTEST_KEY,
         &[*guard, config::PAGE_SIZE, ATTEST_KEY_CURVE_ECC_SECP384R1],
     );
 
     let ret_code = ret[0] as isize;
     let buflen = ret[1] as usize;
     debug!(
-        "GET_REALM_ATTEST_KEY returned with: {}, {}",
+        "RMM_GET_REALM_ATTEST_KEY returned with: {}, {}",
         ret_code, buflen
     );
 
     if ret_code != 0 {
         let e: RmmEl3IfcError = ret_code.into();
-        error!("GET_REALM_ATTEST_KEY failed with {:?}", e);
+        error!("RMM_GET_REALM_ATTEST_KEY failed with {:?}", e);
     }
 
     let v = utils::va_to_vec(*guard, buflen);
     utils::set_vector(v, &REALM_ATTEST_KEY);
 
-    debug!("{:x?}", super::realm_attest_key());
+    debug!("REALM_ATTEST_KEY: {:02x?}", super::realm_attest_key());
 }
 
 pub(super) fn get_plat_token() {
-    trace!("GET_PLAT_TOKEN");
+    trace!("RMM_GET_PLAT_TOKEN");
 
     let guard: SpinlockGuard<'_, _> = RMM_SHARED_BUFFER_LOCK.lock();
 
-    let dak_priv = utils::get_vector(&REALM_ATTEST_KEY);
+    let dak_priv = utils::get_spinlock(&REALM_ATTEST_KEY);
     let dak_pub_hash = digest::get_realm_public_key_hash(dak_priv);
     utils::vec_to_va(&dak_pub_hash, *guard, config::PAGE_SIZE);
 
     let ret = smc(
-        rmi::GET_PLAT_TOKEN,
+        rmi::RMM_GET_PLAT_TOKEN,
         &[*guard, config::PAGE_SIZE, SHA256_DIGEST_SIZE],
     );
 
     let ret_code = ret[0] as isize;
     let buflen = ret[1] as usize;
-    debug!("GET_PLAT_TOKEN returned with: {}, {}", ret_code, buflen);
+    debug!("RMM_GET_PLAT_TOKEN returned with: {}, {}", ret_code, buflen);
 
     if ret_code != 0 {
         let e: RmmEl3IfcError = ret_code.into();
-        error!("GET_PLAT_TOKEN failed with {:?}", e);
+        error!("RMM_GET_PLAT_TOKEN failed with {:?}", e);
     }
 
     let v = utils::va_to_vec(*guard, buflen);
     utils::set_vector(v, &PLAT_TOKEN);
 
-    debug!("{:x?}", super::plat_token());
+    debug!("PLAT_TOKEN: {:02x?}", super::plat_token());
+}
+
+pub(super) fn get_vhuks() {
+    trace!("RMM_ISLET_GET_VHUK(A&M)");
+
+    let ret_code = get_vhuk(ID_VHUK_A, &VHUK_A);
+    if ret_code != 0 {
+        let e: RmmEl3IfcError = ret_code.into();
+        error!("RMM_ISLET_GET_VHUK(A) failed with {:?}", e);
+    }
+    info!("VHUK_A: {:02x?}", super::vhuk_a());
+
+    let ret_code = get_vhuk(ID_VHUK_M, &VHUK_M);
+    if ret_code != 0 {
+        let e: RmmEl3IfcError = ret_code.into();
+        error!("RMM_ISLET_GET_VHUK(M) failed with {:?}", e);
+    }
+    info!("VHUK_M: {:02x?}", super::vhuk_m());
+}
+
+fn get_vhuk(id: usize, out: &Spinlock<[u8; 32]>) -> isize {
+    trace!("RMM_ISLET_GET_VHUK");
+
+    let ret = smc(rmi::RMM_ISLET_GET_VHUK, &[id]);
+
+    let ret_code = ret[0] as isize;
+    debug!("RMM_ISLET_GET_VHUK returned with: {}", ret_code);
+
+    if ret_code != 0 {
+        let e: RmmEl3IfcError = ret_code.into();
+        error!("RMM_ISLET_GET_VHUK failed with {:?}", e);
+    }
+
+    utils::set_array(ret, 1..5, out);
+
+    ret_code
 }

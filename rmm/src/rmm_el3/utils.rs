d349b47fa214910901b0b0658d6a8f5816e1611c
Lukasz Pawelczyk <l.pawelczyk@samsung.com>
2024-09-18 13:41:14 +0200

    Get VHUKS from TF-A using vendor SMC RMM calls
    Signed-off-by: Lukasz Pawelczyk <l.pawelczyk@samsung.com>

--- rmm/src/rmm_el3/utils.rs
+++ rmm/src/rmm_el3/utils.rs
@@ -1,5 +1,5 @@
 use alloc::vec::Vec;
-use core::{ptr, slice};
+use core::{ops::Range, ptr, slice};
 use spinning_top::{Spinlock, SpinlockGuard};
 
 pub(super) fn va_to_vec(ptr: usize, len: usize) -> Vec<u8> {
@@ -26,7 +26,21 @@ pub(super) fn set_vector(src: Vec<u8>, dst: &Spinlock<Vec<u8>>) {
     guard.extend_from_slice(&src);
 }
 
-pub(super) fn get_vector(src: &Spinlock<Vec<u8>>) -> Vec<u8> {
+// it's up to the caller of this function to make sure dst won't go out of bounds
+pub(super) fn set_array<const N: usize>(
+    smc_ret: [usize; 8],
+    range: Range<usize>,
+    dst: &Spinlock<[u8; N]>,
+) {
+    let mut guard: SpinlockGuard<'_, _> = dst.lock();
+
+    let len = core::mem::size_of::<usize>();
+    for (i, reg) in smc_ret[range].iter().enumerate() {
+        guard[i * len..i * len + len].copy_from_slice(reg.to_ne_bytes().as_slice());
+    }
+}
+
+pub(super) fn get_spinlock<T: Clone>(src: &Spinlock<T>) -> T {
     let guard: SpinlockGuard<'_, _> = src.lock();
     guard.clone()
 }

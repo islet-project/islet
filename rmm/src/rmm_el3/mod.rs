ac1794512c4dee755fb19eb667744db52cf2a245
Lukasz Pawelczyk <l.pawelczyk@samsung.com>
2024-09-18 13:41:14 +0200

    Get VHUKS from TF-A using vendor SMC RMM calls
    Signed-off-by: Lukasz Pawelczyk <l.pawelczyk@samsung.com>

--- rmm/src/rmm_el3/mod.rs
+++ rmm/src/rmm_el3/mod.rs
@@ -17,9 +17,13 @@ use spinning_top::Spinlock;
 const SHA256_DIGEST_SIZE: usize = 32;
 const ATTEST_KEY_CURVE_ECC_SECP384R1: usize = 0;
 
+const VHUK_LENGTH: usize = 32;
+
 static RMM_SHARED_BUFFER_LOCK: Spinlock<usize> = Spinlock::new(RMM_SHARED_BUFFER_START);
 static REALM_ATTEST_KEY: Spinlock<Vec<u8>> = Spinlock::new(Vec::new());
 static PLAT_TOKEN: Spinlock<Vec<u8>> = Spinlock::new(Vec::new());
+static VHUK_A: Spinlock<[u8; VHUK_LENGTH]> = Spinlock::new([0xAAu8; VHUK_LENGTH]);
+static VHUK_M: Spinlock<[u8; VHUK_LENGTH]> = Spinlock::new([0x33u8; VHUK_LENGTH]);
 
 pub fn setup_el3_ifc() {
     trace!("Setup EL3 interface");
@@ -27,14 +31,27 @@ pub fn setup_el3_ifc() {
     asm::dcache_flush(RMM_SHARED_BUFFER_START, config::PAGE_SIZE);
     iface::get_realm_attest_key();
     iface::get_plat_token();
+    iface::get_vhuks();
 }
+
+// TODO: should those functions fail when respective RMM from TF-A failed?
 
 #[allow(dead_code)]
 pub fn realm_attest_key() -> Vec<u8> {
-    utils::get_vector(&REALM_ATTEST_KEY)
+    utils::get_spinlock(&REALM_ATTEST_KEY)
 }
 
 #[allow(dead_code)]
 pub fn plat_token() -> Vec<u8> {
-    utils::get_vector(&PLAT_TOKEN)
+    utils::get_spinlock(&PLAT_TOKEN)
+}
+
+#[allow(dead_code)]
+pub fn vhuk_a() -> [u8; VHUK_LENGTH] {
+    utils::get_spinlock(&VHUK_A)
+}
+
+#[allow(dead_code)]
+pub fn vhuk_m() -> [u8; VHUK_LENGTH] {
+    utils::get_spinlock(&VHUK_M)
 }

a9d171d9ae96ffedda05c2c17ff25023b2d36f09
Zofia Abramowska <z.abramowska@samsung.com>
2024-11-13 12:49:17 +0100

    veraison: Freeze veraison/services revision
    ...and hope for the best
    
    Signed-off-by: Zofia Abramowska <z.abramowska@samsung.com>

--- examples/veraison/bootstrap.sh
+++ examples/veraison/bootstrap.sh
@@ -7,6 +7,7 @@ ROOT_DIR=$(git rev-parse --show-toplevel)
 SERVICES_REPO="https://github.com/veraison/services.git"
 SERVICES_DIR="$ROOT_DIR/examples/veraison/services"
 DOCKER_DIR="$SERVICES_DIR/deployments/docker"
+REVISION="736e119"
 
 if [ -d "$SERVICES_DIR" ]
 then
@@ -17,6 +18,9 @@ fi
 go install github.com/veraison/ear/arc@latest
 
 git clone --depth=1 "$SERVICES_REPO" "$SERVICES_DIR"
+pushd "$SERVICES_DIR"
+git checkout -b freeze "$REVISION"
+popd
 
 for patch in "./veraison-no-auth-patch" ; do
     cat "$patch" | (cd "$SERVICES_DIR" && git apply);

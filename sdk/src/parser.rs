c1f6b2f84d2cbb4ce348d0a60de2b37064df402e
Sangwan Kwon <sangwan.kwon@samsung.com>
2024-11-28 16:50:00 +0900

    sdk: Fix to return user_data on the simulated version
    Signed-off-by: Sangwan Kwon <sangwan.kwon@samsung.com>

--- sdk/src/parser.rs
+++ sdk/src/parser.rs
@@ -5,9 +5,10 @@ use rust_rsi::{print_token, PlatClaims, RealmClaims};
 pub fn parse(claims: &AttestationClaims) -> Result<(RealmClaims, PlatClaims), Error> {
     cfg_if::cfg_if! {
         if #[cfg(target_arch = "x86_64")] {
-            let realm_claims = RealmClaims::from_raw_claims(
+            let mut realm_claims = RealmClaims::from_raw_claims(
                     &claims.origin.realm_claims.token_claims,
                     &claims.origin.realm_claims.measurement_claims)?;
+            realm_claims.challenge = claims.user_data.clone();
             let plat_claims = PlatClaims::from_raw_claims(
                     &claims.origin.platform_claims.token_claims)?;
             Ok((realm_claims, plat_claims))

27cfaf12e2dc15c618727441d770d3c9f82496d2
Bokdeuk Jeong <bd.jeong@samsung.com>
2024-12-03 02:06:30 -0500

    rmi_features: use defined_bits() for feature register
    Signed-off-by: Bokdeuk Jeong <bd.jeong@samsung.com>

--- rmm/src/rmi/features.rs
+++ rmm/src/rmi/features.rs
@@ -1,45 +1,35 @@
 use crate::event::RmiHandle;
 use crate::listen;
 use crate::rmi;
+use armv9a::{define_bitfield, define_bits, define_mask};
 
 extern crate alloc;
 
-const S2SZ_SHIFT: usize = 0;
-const S2SZ_WIDTH: usize = 8;
-const S2SZ_VALUE: usize = 48;
+define_bits!(
+    FeatureReg0,
+    HASH_SHA_512[29 - 29],
+    HASH_SHA_256[28 - 28],
+    PMU_NUM_CTRS[27 - 23],
+    PMU_EN[22 - 22],
+    NUM_WPS[21 - 18],
+    NUM_BPS[17 - 14],
+    SVE_VL[13 - 10],
+    SVE_EN[9 - 9],
+    LPA2[8 - 8],
+    S2SZ[7 - 0]
+);
 
-const LPA2_SHIFT: usize = 8;
-#[allow(unused)]
-const LPA2_WIDTH: usize = 1;
-const LPA2_VALUE: usize = 0;
+const S2SZ_VALUE: u64 = 48;
+const LPA2_VALUE: u64 = 0;
+const PMU_EN_VALUE: u64 = NOT_SUPPORTED;
+const PMU_NUM_CTRS_VALUE: u64 = 0;
+const HASH_SHA_256_VALUE: u64 = SUPPORTED;
+const HASH_SHA_512_VALUE: u64 = SUPPORTED;
 
-const PMU_EN_SHIFT: usize = 22;
-const PMU_EN_WIDTH: usize = 1;
-const PMU_EN_VALUE: usize = NOT_SUPPORTED;
-
-const PMU_NUM_CTRS_SHIFT: usize = 23;
-const PMU_NUM_CTRS_WIDTH: usize = 5;
-const PMU_NUM_CTRS_VALUE: usize = 0;
-
-const HASH_SHA_256_SHIFT: usize = 28;
-const HASH_SHA_256_VALUE: usize = SUPPORTED;
-
-const HASH_SHA_512_SHIFT: usize = 29;
-const HASH_SHA_512_VALUE: usize = SUPPORTED;
-
-const NOT_SUPPORTED: usize = 0;
-const SUPPORTED: usize = 1;
+const NOT_SUPPORTED: u64 = 0;
+const SUPPORTED: u64 = 1;
 
 const FEATURE_REGISTER_0_INDEX: usize = 0;
-
-fn extract(reg: usize, shift: usize, width: usize) -> usize {
-    let mask = mask(shift, width);
-    (reg << (mask.trailing_zeros())) & mask
-}
-
-fn mask(shift: usize, width: usize) -> usize {
-    (!0usize >> (64usize - width)) << shift
-}
 
 pub fn set_event_handler(rmi: &mut RmiHandle) {
     listen!(rmi, rmi::FEATURES, |arg, ret, _| {
@@ -48,41 +38,30 @@ pub fn set_event_handler(rmi: &mut RmiHandle) {
             return Ok(());
         }
 
-        let mut feat_reg0: usize = 0;
-        feat_reg0 |= S2SZ_VALUE << S2SZ_SHIFT;
-        feat_reg0 |= LPA2_VALUE << LPA2_SHIFT;
-        feat_reg0 |= PMU_EN_VALUE << PMU_EN_SHIFT;
-        feat_reg0 |= PMU_NUM_CTRS_VALUE << PMU_NUM_CTRS_SHIFT;
-        feat_reg0 |= HASH_SHA_256_VALUE << HASH_SHA_256_SHIFT;
-        feat_reg0 |= HASH_SHA_512_VALUE << HASH_SHA_512_SHIFT;
+        let mut feat_reg0 = FeatureReg0::new(0);
+        feat_reg0
+            .set_masked_value(FeatureReg0::S2SZ, S2SZ_VALUE)
+            .set_masked_value(FeatureReg0::LPA2, LPA2_VALUE)
+            .set_masked_value(FeatureReg0::PMU_EN, PMU_EN_VALUE)
+            .set_masked_value(FeatureReg0::PMU_NUM_CTRS, PMU_NUM_CTRS_VALUE)
+            .set_masked_value(FeatureReg0::HASH_SHA_256, HASH_SHA_256_VALUE)
+            .set_masked_value(FeatureReg0::HASH_SHA_512, HASH_SHA_512_VALUE);
 
-        ret[1] = feat_reg0;
-        debug!("rmi::FEATURES ret:{:X}", feat_reg0);
+        ret[1] = feat_reg0.get() as usize;
+        debug!("rmi::FEATURES ret:{:X}", feat_reg0.get());
         Ok(())
     });
 }
 
-pub fn ipa_bits(feat_reg0: usize) -> usize {
-    extract(feat_reg0, S2SZ_SHIFT, S2SZ_WIDTH)
+pub fn ipa_bits(val: usize) -> usize {
+    let feat_reg0 = FeatureReg0::new(val as u64);
+    feat_reg0.get_masked_value(FeatureReg0::S2SZ) as usize
 }
 
 //TODO: locate validate() in armv9a to check against AA64MMFR_EL1 register
-pub fn validate(feat_reg0: usize) -> bool {
-    const MIN_IPA_SIZE: usize = 32;
-    let s2sz = extract(feat_reg0, S2SZ_SHIFT, S2SZ_WIDTH);
+pub fn validate(s2sz: u64) -> bool {
+    const MIN_IPA_SIZE: u64 = 32;
     if !(MIN_IPA_SIZE..=S2SZ_VALUE).contains(&s2sz) {
-        return false;
-    }
-
-    if extract(feat_reg0, S2SZ_SHIFT, S2SZ_WIDTH) > S2SZ_VALUE {
-        return false;
-    }
-
-    // TODO: Add a check for LPA2 flag with AA64MMFR_EL1 reigster after refactoring
-
-    if extract(feat_reg0, PMU_EN_SHIFT, PMU_EN_WIDTH) == SUPPORTED
-        && extract(feat_reg0, PMU_NUM_CTRS_SHIFT, PMU_NUM_CTRS_WIDTH) != PMU_NUM_CTRS_VALUE
-    {
         return false;
     }
 

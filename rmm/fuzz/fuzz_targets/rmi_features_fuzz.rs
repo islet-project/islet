7d160135629d7fc67248ad23f828154b0702e91e
Rayhan Faizel <rayhan.faizel@gmail.com>
2025-02-10 03:26:55 +0530

    rmm: Add fuzzing harnesses for RMI commands: FEATURES and VERSION
    Signed-off-by: Rayhan Faizel <rayhan.faizel@gmail.com>

---
+++ rmm/fuzz/fuzz_targets/rmi_features_fuzz.rs
@@ -0,0 +1,11 @@
+#![no_main]
+
+use islet_rmm::rmi::{FEATURES};
+use islet_rmm::test_utils::*;
+
+use libfuzzer_sys::fuzz_target;
+
+fuzz_target!(|data: u64| {
+    let feature_reg_index = data as usize;
+    let _ret = rmi::<FEATURES>(&[feature_reg_index]);
+});

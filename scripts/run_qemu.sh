96b2a9b50415e113c8794df3308ba9f7db84ab81
Sunwook Eom <speed.eom@samsung.com>
2025-03-11 16:29:56 +0900

    script: android: run qemu with TF-A & ramdisk
    To launch realm in the android, we need kvmtool and TF-A & TF-RMM.
    So we need the following images:
    - aosp_rme_ramdisk : It includes kvmtool in Virtualization module of aosp
    - flash.bin: It includes TF-A & TF-RMM images
    Those images are already pushed in the islet-project/3rd-android-kernel. So we don't need any other command for them.
    
    But if you want to launch realm in android, please follows the below steps:
    1. adb root
    2. push your guest rootfs and guest kernel image to /mnt
    3. run kvmtool in android
    (e.g, /apex/com.android.virt/bin/lkvm run --debug --realm -c 2 -m 2G -k /mnt/Image -d /mnt/rootfs.ext4 --restricted_mem -p "console=hvc0 root=/dev/vda")
    
    FYI, The flash.bin(TF-A bin) is from linaro's repository.
    For more information, see the following link:
    https://linaro.atlassian.net/wiki/spaces/QEMU/pages/29596450823/Manual+build+instructions+for+TF-RMM+TF-A+and+host+EDK2+for+QEMU-virt
    
    Signed-off-by: Sunwook Eom <speed.eom@samsung.com>

--- scripts/run_qemu.sh
+++ scripts/run_qemu.sh
@@ -4,6 +4,17 @@ ISLET_DIR=$1
 AOSP_DIR=$2
 INITRAMFS_PATH=$3
 KERNEL_PATH=$4
+AOSP_KERNEL_SRC_DIR_PATH=$5
+AOSP_RME_RAMDISK_PATH="$(realpath $AOSP_KERNEL_SRC_DIR_PATH/aosp_rme_ramdisk.img)"
+# This flash.bin is from linaro's repository.
+# For more information, see the following link:
+# https://linaro.atlassian.net/wiki/spaces/QEMU/pages/29596450823/Manual+build+instructions+for+TF-RMM+TF-A+and+host+EDK2+for+QEMU-virt
+TF_A_TF_RMM_IMAGE_PATH="$(realpath $AOSP_KERNEL_SRC_DIR_PATH/flash.bin)"
+KERNEL_CMDLINE="androidboot.hypervisor.vm.supported=1 vmw_vsock_virtio_transport_common.virtio_transport_max_vsock_pkt_buf_size=16384 console=ttynull stack_depot_disable=on cgroup_disable=pressure kasan.stacktrace=off bootconfig  printk.devkmsg=on audit=1 panic=-1 8250.nr_uarts=1 cma=0 firmware_class.path=/vendor/etc/ loop.max_part=7 init=/init bootconfig  console=hvc0 earlycon=pl011,mmio32,0x9000000 "
+
+KERNEL_CMDLINE+=$AOSP_RME_RAMDISK_PATH
+
+
 
 function run_qemu()
 {
@@ -20,8 +31,11 @@ function run_qemu()
 	# Run cuttlefish with cca support linux -> after start kernel, there is no logs..
 	echo "Running Cuttlefish based by QEMU..."
 	launch_cvd -vm_manager qemu_cli -enable_host_bluetooth false -report_anonymous_usage_stats=n \
+		--memory_mb 8192 \
 		-initramfs_path $INITRAMFS_PATH \
-		-kernel_path $KERNEL_PATH
+		-kernel_path $KERNEL_PATH \
+		-extra_kernel_cmdline $KERNEL_CMDLINE \
+		-bootloader $TF_A_TF_RMM_IMAGE_PATH
 }
 
 run_qemu

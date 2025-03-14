6ac5ba320bc65a6ca7dbc1e652cec469051b8cb9
Sunwook Eom <speed.eom@samsung.com>
2025-03-14 17:35:55 +0900

    script: qemu-cca: add -aosp-prebuilt option
    Signed-off-by: Sunwook Eom <speed.eom@samsung.com>

--- scripts/run_qemu.sh
+++ scripts/run_qemu.sh
@@ -4,18 +4,16 @@ ISLET_DIR=$1
 AOSP_DIR=$2
 INITRAMFS_PATH=$3
 KERNEL_PATH=$4
-AOSP_KERNEL_SRC_DIR_PATH=$5
-AOSP_RME_RAMDISK_PATH="$(realpath $AOSP_KERNEL_SRC_DIR_PATH/aosp_rme_ramdisk.img)"
+AOSP_RME_RAMDISK_PATH=$5
 # This flash.bin is from linaro's repository.
 # For more information, see the following link:
 # https://linaro.atlassian.net/wiki/spaces/QEMU/pages/29596450823/Manual+build+instructions+for+TF-RMM+TF-A+and+host+EDK2+for+QEMU-virt
-TF_A_TF_RMM_IMAGE_PATH="$(realpath $AOSP_KERNEL_SRC_DIR_PATH/flash.bin)"
+TF_A_IMAGE_PATH=$6
 KERNEL_CMDLINE="androidboot.hypervisor.vm.supported=1 vmw_vsock_virtio_transport_common.virtio_transport_max_vsock_pkt_buf_size=16384 console=ttynull stack_depot_disable=on cgroup_disable=pressure kasan.stacktrace=off bootconfig  printk.devkmsg=on audit=1 panic=-1 8250.nr_uarts=1 cma=0 firmware_class.path=/vendor/etc/ loop.max_part=7 init=/init bootconfig  console=hvc0 earlycon=pl011,mmio32,0x9000000 "
 
 KERNEL_CMDLINE+=$AOSP_RME_RAMDISK_PATH
 
-function run_qemu()
-{
+function run_qemu() {
 	cd "$ISLET_DIR" || exit
 	# Go to the aosp source directory which you were built before
 	echo "Changing directory to $AOSP_DIR to run qemu..."
@@ -33,7 +31,7 @@ function run_qemu()
 		-initramfs_path $INITRAMFS_PATH \
 		-kernel_path $KERNEL_PATH \
 		-extra_kernel_cmdline $KERNEL_CMDLINE \
-		-bootloader $TF_A_TF_RMM_IMAGE_PATH
+		-bootloader $TF_A_IMAGE_PATH
 }
 
 run_qemu

1f107cc043b6731fa7ffed9157bb2505f476beb7
Sunwook Eom <speed.eom@samsung.com>
2025-03-19 14:35:24 +0900

    script: refactor: add exit codes in init_android_on_qemu.sh
    Signed-off-by: Sunwook Eom <speed.eom@samsung.com>

--- scripts/init_android_on_qemu.sh
+++ scripts/init_android_on_qemu.sh
@@ -24,6 +24,14 @@ KERNEL_PATH="$ANDROID_KERNEL_DIR/out/virtual_device_aarch64/dist/Image"
 QEMU_REPO="https://gitlab.com/qemu-project/qemu.git"
 QEMU_TAG="v9.1.2"
 
+# Exit codes
+EXIT_CD_FAILED=1
+EXIT_REPO_DOWNLOAD=10
+EXIT_AOSP_BUILD=20
+EXIT_KERNEL_BUILD=30
+EXIT_QEMU_BUILD=40
+EXIT_OTHER=125
+
 function install_required_packages() {
 	if [ -z "$(which repo)" ]; then
 		sudo apt-get install repo
@@ -31,27 +39,26 @@ function install_required_packages() {
 }
 
 function build_aosp() {
-	cd "$ISLET_DIR" || exit
+	cd "$ISLET_DIR" || exit $EXIT_CD_FAILED # if cd failed, exit with error code
 
-	# Create aosp directory and download AOSP sources
 	if [ -d "$AOSP_DIR" ]; then
 		echo "$AOSP_DIR already exists."
 
 		echo "Changing directory to $AOSP_DIR..."
-		cd $AOSP_DIR || exit 1 # if cd failed, exit with error code
+		cd $AOSP_DIR || exit $EXIT_CD_FAILED
 	else
 		echo "Creating directory $AOSP_DIR..."
 		mkdir -p $AOSP_DIR
 
 		echo "Changing directory to $AOSP_DIR..."
-		cd $AOSP_DIR || exit 2 # if cd failed, exit with error code
+		cd $AOSP_DIR || exit $EXIT_CD_FAILED
 
 		echo "Downloading AOSP sources..."
 		repo init --partial-clone -b $AOSP_BRANCH -u $AOSP_URL
 
 		if ! repo sync -c -j8; then
-			echo "ERROR: Download AOSP failed"
-			exit 3
+			echo "ERROR: AOSP sync failed"
+			exit $EXIT_REPO_DOWNLOAD
 		fi
 	fi
 
@@ -68,16 +75,16 @@ function build_aosp() {
 
 	echo "Building AOSP..."
 	if ! m; then
-		echo "ERROR: AOSP Build failed"
-		exit 4
+		echo "ERROR: AOSP build failed"
+		exit $EXIT_AOSP_BUILD
 	fi
 
 	echo "Go back to $ISLET_DIR..."
-	cd "$ISLET_DIR" || exit 5 # if cd failed, exit with error code
+	cd "$ISLET_DIR" || exit $EXIT_CD_FAILED
 }
 
 function build_android_kernel() {
-	cd "$ISLET_DIR" || exit
+	cd "$ISLET_DIR" || exit $EXIT_CD_FAILED
 
 	if [ ! -d "$ANDROID_KERNEL_DIR" ]; then
 		echo "Creating directory $ANDROID_KERNEL_DIR..."
@@ -85,19 +92,19 @@ function build_android_kernel() {
 	fi
 
 	echo "Changing directory to $ANDROID_KERNEL_DIR..."
-	cd $ANDROID_KERNEL_DIR || exit 1
+	cd $ANDROID_KERNEL_DIR || exit $EXIT_CD_FAILED
 
 	if [ ! -d "common" ]; then
 		echo "unzip repo.zip to run 'repo sync'"
 		if ! unzip repo.zip; then
 			echo "ERROR: unzip repo.zip failed"
-			exit 2
+			exit $EXIT_OTHER
 		fi
 
 		echo "Downloading Android Kernel Build Modules..."
 		if ! repo sync; then
-			echo "ERROR: Failed to download Android Kernel Build Modules..."
-			exit 3
+			echo "ERROR: Kernel sync failed"
+			exit $EXIT_REPO_DOWNLOAD
 		fi
 	fi
 
@@ -105,9 +112,9 @@ function build_android_kernel() {
 		echo "Replace common with cca patched kernel sources..."
 		mv common backup_common
 		if ! git clone $ANDROID_KERNEL_URL -b $ANDROID_KERNEL_SOURCE_BRANCH --depth 1 --single-branch common; then
-			echo "ERROR: Failed to Download Android Kernel Source failed"
+			echo "ERROR: Kernel source clone failed"
 			mv backup_common common
-			exit 4
+			exit $EXIT_REPO_DOWNLOAD
 		fi
 	fi
 
@@ -116,11 +123,10 @@ function build_android_kernel() {
 		return
 	fi
 
-	# Build
 	echo "Building Android Kernel..."
 	if ! tools/bazel run $ANDROID_KERNEL_BUILD_TARGET; then
-		echo "ERROR: Android Kernel Build failed"
-		exit 5
+		echo "ERROR: Kernel build failed"
+		exit $EXIT_KERNEL_BUILD
 	fi
 
 	echo "Check built images..."
@@ -128,35 +134,38 @@ function build_android_kernel() {
 	realpath $KERNEL_PATH
 
 	echo "Go back to $ISLET_DIR..."
-	cd $ISLET_DIR || exit 6
+	cd $ISLET_DIR || exit $EXIT_CD_FAILED
 }
 
 function clone_qemu_source() {
-	cd "$QEMU_RME_DIR" || exit 1
+	cd "$QEMU_RME_DIR" || exit $EXIT_CD_FAILED
 
 	if [ ! -d $QEMU_SRC_DIR ]; then
 		echo "Cloning QEMU repository with tag $QEMU_TAG..."
 		git clone --single-branch -b "$QEMU_TAG" --depth 1 "$QEMU_REPO" qemu
 	fi
-	cd qemu || exit 1
+	cd qemu || exit $EXIT_CD_FAILED
 
 	# check the tag	hash
 	TAG_HASH=$(git rev-list -n 1 "$QEMU_TAG")
 	if [ -z "$TAG_HASH" ]; then
-		echo "Error: Tag $QEMU_TAG not found"
-		exit 1
+		echo "ERROR: QEMU tag $QEMU_TAG not found"
+		exit $EXIT_OTHER
 	fi
 }
 
 function build_qemu() {
-	cd "$QEMU_SRC_DIR" || exit 1
+	cd "$QEMU_SRC_DIR" || exit $EXIT_CD_FAILED
 
 	if [ ! -d "build" ]; then
 		echo "Configuring QEMU..."
 		./configure --target-list=aarch64-softmmu --enable-slirp --disable-docs
 
 		echo "Building QEMU..."
-		make -j$(nproc)
+		if ! make -j$(nproc); then
+			echo "ERROR: QEMU build failed"
+			exit $EXIT_QEMU_BUILD
+		fi
 	else
 		echo "QEMU is already built. Skip building QEMU"
 	fi

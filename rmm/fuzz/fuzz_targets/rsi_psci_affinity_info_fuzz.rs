dc6430c2db2905cc2941d4ca6bec505aa13ef93e
Rayhan Faizel <rayhan.faizel@gmail.com>
2025-04-11 01:44:21 +0530

    rmm/fuzz: Add rsi_psci_cpu_on_fuzz and rsi_psci_affinity_info_fuzz
    Signed-off-by: Rayhan Faizel <rayhan.faizel@gmail.com>

---
+++ rmm/fuzz/fuzz_targets/rsi_psci_affinity_info_fuzz.rs
@@ -0,0 +1,48 @@
+#![no_main]
+
+use islet_rmm::rec::Rec;
+use islet_rmm::rmi::rec::run::Run;
+use islet_rmm::rmi::{PSCI_COMPLETE, REC_ENTER, SUCCESS, SUCCESS_REC_ENTER};
+use islet_rmm::rsi::PSCI_AFFINITY_INFO;
+use islet_rmm::test_utils::{mock, *};
+
+use libfuzzer_sys::{arbitrary, fuzz_target};
+
+#[derive(Debug, arbitrary::Arbitrary)]
+struct PSCIAffinityFuzz {
+    target_affinity: u64,
+    lowest_affinity_level: u32,
+    status: usize,
+    target_runnable: bool,
+}
+
+fuzz_target!(|data: PSCIAffinityFuzz| {
+    let rd = mock::host::realm_setup();
+    let target_affinity = data.target_affinity as usize;
+    let lowest_affinity_level = data.lowest_affinity_level as usize;
+    let status = data.status;
+    let target_runnable = data.target_runnable as u64;
+
+    let (rec1, run1) = (alloc_granule(IDX_REC1), alloc_granule(IDX_REC1_RUN));
+
+    let rec2 = alloc_granule(IDX_REC2);
+
+    let ret = rmi::<REC_ENTER>(&[
+        rec1,
+        run1,
+        PSCI_AFFINITY_INFO,
+        target_affinity,
+        lowest_affinity_level,
+    ]);
+
+    if ret[0] == SUCCESS {
+        unsafe {
+            let target_rec = &mut *(rec2 as *mut Rec<'_>);
+            target_rec.set_runnable(target_runnable);
+        }
+
+        let _ret = rmi::<PSCI_COMPLETE>(&[rec1, rec2, status]);
+    }
+
+    mock::host::realm_teardown(rd);
+});

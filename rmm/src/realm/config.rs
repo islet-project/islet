133fcd7372e425c194756e4adee061b808a71cdd
Changho Choi <ch754.choi@samsung.com>
2025-06-12 14:07:44 +0900

    Add rpv in realm config structure
    This adds rpv in RealmConfig structure.
    
    B5.4.5 RsiRealmConfig type
    Name      Byte offset Type              Description
    ipa_width 0x0         UInt64            IPA width in bits
    hash_algo 0x8         RsiHashAlgorithm  Hash algorithm
    rpv       0x200       Bits512           Realm Personalization Value
    
    Signed-off-by: Changho Choi <ch754.choi@samsung.com>

--- rmm/src/realm/config.rs
+++ rmm/src/realm/config.rs
@@ -1,14 +1,16 @@
 use crate::realm::mm::address::GuestPhysAddr;
 use crate::realm::mm::rtt::RTT_PAGE_LEVEL;
-use crate::realm::rd::Rd;
+use crate::realm::rd::{Rd, RPV_SIZE};
 use crate::rmi::error::Error;
 
 use safe_abstraction::raw_ptr::assume_safe;
 
 #[repr(C)]
 pub struct RealmConfig {
-    ipa_width: usize,
-    hash_algo: u8,
+    ipa_width: usize,    // Offset 0x0
+    hash_algo: u8,       // Offset 0x8
+    dummy: [u8; 0x1F7],  // 0x1F7 == 0x200 - 0x9 (offset 0x8 + 1 byte)
+    rpv: [u8; RPV_SIZE], // Offset 0x200
 }
 
 impl RealmConfig {
@@ -19,14 +21,15 @@ impl RealmConfig {
     // in parsing the following kernel cmdline argument:
     // `console=ttyS0 root=/dev/vda rw  console=pl011,mmio,0x1c0a0000 console=ttyAMA0 printk.devkmsg=on`.
     // So, we get back to use the same kernel argument with TF-RMM's one (uart0 & uart3).
-    pub fn init(config_addr: usize, ipa_width: usize, hash_algo: u8) -> Result<(), Error> {
+    pub fn init(config_addr: usize, ipa_width: usize, hash_algo: u8, rpv: &[u8]) -> Result<(), Error> {
         Ok(assume_safe::<RealmConfig>(config_addr)
-           .map(|mut realm_config| realm_config.init_inner(ipa_width, hash_algo))?)
+           .map(|mut realm_config| realm_config.init_inner(ipa_width, hash_algo, rpv))?)
     }
 
-    fn init_inner(&mut self, ipa_width: usize, hash_algo: u8) {
+    fn init_inner(&mut self, ipa_width: usize, hash_algo: u8, rpv: &[u8]) {
         self.ipa_width = ipa_width;
         self.hash_algo = hash_algo;
+        self.rpv.copy_from_slice(rpv);
     }
 }
 
@@ -36,8 +39,9 @@ pub fn realm_config(rd: &Rd, config_ipa: usize, ipa_bits: usize) -> Result<(), E
         .lock()
         .ipa_to_pa(GuestPhysAddr::from(config_ipa), RTT_PAGE_LEVEL);
     let hash_algo = rd.hash_algo();
+    let rpv = rd.personalization_value();
     if let Some(pa) = res {
-        RealmConfig::init(pa.into(), ipa_bits, hash_algo)
+        RealmConfig::init(pa.into(), ipa_bits, hash_algo, rpv)
     } else {
         Err(Error::RmiErrorInput)
     }

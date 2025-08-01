b449b0a35f2b720bc98cfa4d346f3065bc4fca4f
Bokdeuk Jeong <bd.jeong@samsung.com>
2025-08-01 01:02:56 -0400

    scripts: use prebuilt edk2 and don't clone qemu submodules
    To save storage space and time for compilation,
    use prebuilt edk stored in assets/prebuilt
    and don't clone qemu's submodules, which optee-build doesn't use.
    
    Signed-off-by: Bokdeuk Jeong <bd.jeong@samsung.com>

--- scripts/deps/submodule.sh
+++ scripts/deps/submodule.sh
@@ -4,4 +4,10 @@ set -e
 
 ROOT=$(git rev-parse --show-toplevel)
 
-cd $ROOT && git submodule update --init --depth 1 --recursive
+cd $ROOT && git submodule update --init --depth 1
+echo $PWD $ROOT
+cd $ROOT && git submodule foreach '
+    if [ "$(basename $path)" != "qemu" ]; then
+        git submodule update --init --depth 1 --recursive
+    fi
+    true'

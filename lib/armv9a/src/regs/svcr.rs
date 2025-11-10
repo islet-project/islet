9ba2680f294921f4a64bd22c4ccd2596ee3bf3b6
Bokdeuk Jeong <bd.jeong@samsung.com>
2025-11-10 03:41:42 -0500

    update rmm build option after the rustc update
    1. Remove 'sme' from the build features. Otherwise, rustc tries to
       utilize simd registers in rmm and it results in corrupting both
       rmm and realm states since Islet doesn't maintain simd register
       contexts for its own by choice.
    2. Call sve registers by op codes instead of their mnemonics,
       since compiler does not understand the names anymore.
    
    Signed-off-by: Bokdeuk Jeong <bd.jeong@samsung.com>

--- lib/armv9a/src/regs/svcr.rs
+++ lib/armv9a/src/regs/svcr.rs
@@ -1,9 +1,6 @@
-// SPDX-License-Identifier: Apache-2.0 OR MIT
-//
-
-//! AArch64 Processor Feature Register 1 - EL1
+//! Streaming Vector Control Register
 //!
-//! Provides additional information about implemented PE features in AArch64 state.
+//! Controls Streaming SVE mode and SME behavior.
 
 use tock_registers::{
     interfaces::{Readable, Writeable},
@@ -25,13 +22,17 @@ impl Readable for Reg {
     type T = u64;
     type R = SVCR::Register;
 
-    sys_coproc_read_raw!(u64, "SVCR", "x");
+    // Use the opcode instead of its register mnemonic
+    // to pass compilation without SIMD(neon, sve, sme) features in the compile option
+    //sys_coproc_read_raw!(u64, "SVCR", "x");
+    sys_coproc_read_raw!(u64, "S3_3_C4_C2_2", "x");
 }
 
 impl Writeable for Reg {
     type T = u64;
     type R = SVCR::Register;
 
-    sys_coproc_write_raw!(u64, "SVCR", "x");
+    //sys_coproc_write_raw!(u64, "SVCR", "x");
+    sys_coproc_write_raw!(u64, "S3_3_C4_C2_2", "x");
 }
 pub const SVCR: Reg = Reg {};

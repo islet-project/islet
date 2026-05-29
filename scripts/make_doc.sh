b767f667283ea364e50df639daee75160d89b657
Piotr Sawicki <p.sawicki2@samsung.com>
2026-05-29 08:53:51 +0200

    Add Islet HES Rust documentation
    Also the make_doc.sh script has been adjusted to the
    new structure of the doc/ folder.
    
    Signed-off-by: Piotr Sawicki <p.sawicki2@samsung.com>

--- scripts/make_doc.sh
+++ scripts/make_doc.sh
@@ -3,21 +3,11 @@
 set -e
 
 ROOT=$(git rev-parse --show-toplevel)
-
-# Link READMEs
-cd ${ROOT}/doc
-ln -sf ../README.md intro.md
-cd ${ROOT}/doc/components
-ln -sf ../../sdk/README.md sdk.md
-ln -sf ../../cli/README.md cli.md
-ln -sf ../../rmm/README.md rmm.md
-cd ${ROOT}/doc/usecases
-ln -sf ../../examples/confidential-ml/README.md confidential-ml.md
-ln -sf ../../examples/cross-platform-e2ee/README.md cross-platform-e2ee.md
-ln -sf ../../examples/veraison/README.md remote-attestation.md
+BOOK=${ROOT}/out/book
 
 # RMM crate
 rm -rf ${ROOT}/doc/plat-doc
+cd ${ROOT}
 cargo doc --lib --no-deps -p islet_rmm
 cp -R ${ROOT}/out/aarch64-unknown-none-softfloat/doc ${ROOT}/doc/plat-doc
 
@@ -27,7 +17,14 @@ cd ${ROOT}/sdk
 cargo doc --lib --no-deps -p islet_sdk
 cp -R ${ROOT}/out/x86_64-unknown-linux-gnu/doc ${ROOT}/doc/app-doc
 
-rm -rf ${ROOT}/book
+
+# HES crate
+rm -rf ${ROOT}/doc/hes-doc
+cd ${ROOT}/hes
+cargo doc --lib --no-deps -p islet-hes
+cp -R ${ROOT}/out/x86_64-unknown-linux-gnu/doc ${ROOT}/doc/hes-doc
+
+rm -rf ${BOOK}
 cd ${ROOT}/doc
 mdbook build
 
@@ -37,3 +34,4 @@ mv ${ROOT}/out/book ${ROOT}/docs
 # Clear
 rm -rf ${ROOT}/doc/plat-doc
 rm -rf ${ROOT}/doc/app-doc
+rm -rf ${ROOT}/doc/hes-doc
